e7998f932702a94c91b943eea38c247e56d82fc3
louthy <p.louth@gmail.com>
2014-11-18 22:17:42 +0000

    Non-null unit tests

---
+++ LanguageExt.Tests/NonNullTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using LanguageExt;
+using LanguageExt.Prelude;
+using NUnit.Framework;
+
+namespace LanguageExtTests
+{
+    [TestFixture]
+    public class NonNullTests
+    {
+        [Test]
+        public void ValueCastTest1()
+        {
+            Assert.Throws(
+                typeof(ValueIsNullException),
+                () =>
+                {
+                    Foo(null);
+                }
+            );
+
+            Assert.Throws(
+                typeof(ValueIsNullException),
+                () =>
+                {
+                    string isnull = null;
+                    Foo(isnull);
+                }
+            );
+        }
+
+        [Test]
+        public void ValueCastTest2()
+        {
+            // These should pass
+            Foo("Hello");
+            string world = "World";
+            Foo(world);
+        }
+
+        [Test]
+        public void NotNullReferenceTypeTest()
+        {
+            Assert.Throws(
+                typeof(ValueIsNullException),
+                () =>
+                {
+                    Some<string> str = null;
+                });
+        }
+
+        private Option<string> GetValue(bool select) =>
+            select
+                ? Some("Hello")
+                : None;
+
+        public void Foo( Some<string> value )
+        {
+            if (value.Value == null)
+            {
+                failwith<Unit>("Value should never be null");
+            }
+
+            string doesItImplicitlyCastBackToAString = value;
+        }
+
+        public void Greet(Some<string> arg)
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
