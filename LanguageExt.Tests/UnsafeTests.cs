337700cc7ce1d88663b136bb37ecd4c2a65da412
louthy <p.louth@gmail.com>
2014-11-20 16:36:31 +0000

    'Unsafe' unit test nuget pkg

---
+++ LanguageExt.Tests/UnsafeTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LanguageExt;
+using LanguageExt.Prelude;
+
+namespace LanguageExtTests
+{
+    [TestFixture]
+    public class UnsafeTests
+    {
+        [Test]
+        public void AssignmentTests()
+        {
+            Option<string> str = SomeUnsafe((string)null);
+
+            string res = match(
+                            str,
+                            Some: v => v,
+                            None: () => "failed"
+                         );
+        }
+    }
+}
