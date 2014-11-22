38a3dc06ce2425b943242062dd541ff5fa0a3bac
louthy <p.louth@gmail.com>
2014-11-22 17:10:41 +0000

    Slightly concerned about the potential cost of 'freezing' the IEnumerable result from a lot of the LINQ calls in List.  It's unlikely to be free.
    So they now just return the IEnumerable.  They are still immutable in the sense that there's no methods for mutation on an IEnumerable.  The programmer will now need to call 'freeze' to turn the result back into an IImmutableList' if they need to call 'add' or 'remove' for example.

--- LanguageExt.Core/List.cs
+++ LanguageExt.Core/List.cs
@@ -20,49 +20,49 @@ namespace LanguageExt
         public static IImmutableList<T> removeAt<T>(this IImmutableList<T> list, int index) =>
             list.RemoveAt(index);
 
-        public static T head<T>(this IImmutableList<T> list) => list.First();
+        public static T head<T>(this IEnumerable<T> list) => list.First();
 
-        public static Option<T> headSafe<T>(this IImmutableList<T> list) =>
+        public static Option<T> headSafe<T>(this IEnumerable<T> list) =>
             list.Take(1).Count() == 1
                 ? Some(list.First())
                 : None;
 
-        public static IImmutableList<T> tail<T>(this IImmutableList<T> list) =>
-            freeze( list.Skip(1) );
+        public static IEnumerable<T> tail<T>(this IEnumerable<T> list) =>
+            list.Skip(1);
 
-        public static IImmutableList<R> map<T, R>(this IImmutableList<T> list, Func<T, R> map) =>
-            freeze( list.Select(map) );
+        public static IEnumerable<R> map<T, R>(this IEnumerable<T> list, Func<T, R> map) =>
+            list.Select(map);
 
-        public static IImmutableList<R> mapi<T, R>(this IImmutableList<T> list, Func<int, T, R> map) =>
-            freeze(zip(list, range(0, Int32.MaxValue), (t, i) => map(i, t)));
+        public static IEnumerable<R> mapi<T, R>(this IEnumerable<T> list, Func<int, T, R> map) =>
+            zip(list, range(0, Int32.MaxValue), (t, i) => map(i, t));
 
-        public static IImmutableList<T> filter<T>(this IImmutableList<T> list, Func<T, bool> predicate) =>
-            freeze( list.Where(predicate) );
+        public static IEnumerable<T> filter<T>(this IEnumerable<T> list, Func<T, bool> predicate) =>
+            list.Where(predicate);
 
-        public static int sum(this IImmutableList<int> list) => fold(list, 0, (x, s) => s + x);
+        public static int sum(this IEnumerable<int> list) => fold(list, 0, (x, s) => s + x);
 
-        public static float sum(this IImmutableList<float> list) => fold(list, 0.0f, (x, s) => s + x);
+        public static float sum(this IEnumerable<float> list) => fold(list, 0.0f, (x, s) => s + x);
 
-        public static double sum(this IImmutableList<double> list) => fold(list, 0.0, (x, s) => s + x);
+        public static double sum(this IEnumerable<double> list) => fold(list, 0.0, (x, s) => s + x);
 
-        public static decimal sum(this IImmutableList<decimal> list) => fold(list, (decimal)0, (x, s) => s + x);
+        public static decimal sum(this IEnumerable<decimal> list) => fold(list, (decimal)0, (x, s) => s + x);
 
-        public static IImmutableList<T> rev<T>(this IImmutableList<T> list) =>
-            freeze( list.Reverse() );
+        public static IEnumerable<T> rev<T>(this IEnumerable<T> list) =>
+            list.Reverse();
 
-        public static IImmutableList<T> append<T>(this IImmutableList<T> lhs, IImmutableList<T> rhs) =>
-            freeze( lhs.Concat(rhs) );
+        public static IEnumerable<T> append<T>(this IEnumerable<T> lhs, IEnumerable<T> rhs) =>
+            lhs.Concat(rhs);
 
-        public static S fold<S, T>(this IImmutableList<T> list, S state, Func<T, S, S> folder)
+        public static S fold<S, T>(this IEnumerable<T> list, S state, Func<T, S, S> folder)
         {
             iter(list, item => { state = folder(item, state); } );
             return state;
         }
 
-        public static S foldr<S, T>(this IImmutableList<T> list, S state, Func<T, S, S> folder) =>
+        public static S foldr<S, T>(this IEnumerable<T> list, S state, Func<T, S, S> folder) =>
             fold(rev(list), state, folder);
 
-        public static T reduce<T>(this IImmutableList<T> list, Func<T, T, T> reducer) =>
+        public static T reduce<T>(this IEnumerable<T> list, Func<T, T, T> reducer) =>
             match(headSafe(list),
                 Some: x => fold(tail(list), x, reducer),
                 None: () => failwith<T>("Input list was empty")
@@ -71,20 +71,11 @@ namespace LanguageExt
         public static IImmutableList<T> freeze<T>(this IEnumerable<T> self) =>
             self.ToImmutableList();
 
-        public static IImmutableList<V> zip<T, U, V>(this IImmutableList<T> self, IEnumerable<U> other, Func<T, U, V> zipper) =>
+        public static IEnumerable<V> zip<T, U, V>(this IEnumerable<T> self, IEnumerable<U> other, Func<T, U, V> zipper) =>
             freeze(self.Zip(other, zipper));
 
-        public static int length<T>(this IImmutableList<T> self) =>
-           self.Count;
-
-        public static Unit iter<T>(this IImmutableList<T> list, Action<T> action)
-        {
-            foreach (var item in list)
-            {
-                action(item);
-            }
-            return unit;
-        }
+        public static int length<T>(this IEnumerable<T> self) =>
+           self.Count();
 
         public static Unit iter<T>(this IEnumerable<T> list, Action<T> action)
         {
@@ -95,18 +86,15 @@ namespace LanguageExt
             return unit;
         }
 
-        public static Unit iteri<T>(this IEnumerable<T> list, Action<int,T> action)
+        public static Unit iteri<T>(this IEnumerable<T> list, Action<int, T> action)
         {
             int i = 0;
             foreach (var item in list)
             {
-                action(i++,item);
+                action(i++, item);
             }
             return unit;
         }
-
-        public static bool forall<T>(this IImmutableList<T> list, Func<T, bool> pred) =>
-            list.All(pred);
 
         public static bool forall<T>(this IEnumerable<T> list, Func<T, bool> pred) =>
             list.All(pred);
