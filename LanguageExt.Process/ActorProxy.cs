d4b1ec241c4a850f338e3112591244792c74a3a4
louthy <p.louth@gmail.com>
2014-12-02 19:09:16 +0000

    Actor proxy

---
+++ LanguageExt.Process/ActorProxy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using LanguageExt.Prelude;
+using LanguageExt.List;
+
+namespace LanguageExt
+{
+    /// <summary>
+    /// ActorProxy
+    /// This class will eventually be responsible for remote messaging
+    /// </summary>
+    internal class ActorProxy : Actor<ActorProxyConfig, object>
+    {
+        public ActorProxy(ProcessId parent, ProcessName name, Func<ActorProxyConfig, object, ActorProxyConfig> actor, Func<ActorProxyConfig> setup)
+            :
+            base(parent,name,actor,setup)
+        {
+        }
+    }
+
+    internal class ActorProxyConfig
+    {
+        public ActorProxyConfig(ProcessId dest)
+        {
+            Destination = dest;
+        }
+
+        public ProcessId Destination { get; }
+    }
+
+    internal static class ActorProxyTemplate
+    {
+        public static Func<ActorProxyConfig, object, ActorProxyConfig> Registered => 
+            (config,msg) =>
+            {
+                Process.tell(config.Destination, msg, Process.sender());
+                return config;
+            };
+    }
+}
