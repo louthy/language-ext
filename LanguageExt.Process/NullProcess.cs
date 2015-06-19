5578b8d14259e52c8bc3e50559b9e137358ad24e
Paul Louth <plouth@meddbase.com>
2015-06-19 18:18:35 +0100

    Fixup actor system to not have a null root process

---
+++ LanguageExt.Process/NullProcess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageExt
+{
+    public class NullProcess : IProcess
+    {
+        public IEnumerable<ProcessId> Children
+        {
+            get
+            {
+                return new ProcessId[0];
+            }
+        }
+
+        public ProcessId Id
+        {
+            get
+            {
+                return ProcessId.None;
+            }
+        }
+
+        public ProcessName Name
+        {
+            get
+            {
+                return "$";
+            }
+        }
+
+        public ProcessId Parent
+        {
+            get
+            {
+                return ProcessId.None;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Unit Restart()
+        {
+            return Unit.Default;
+        }
+
+        public Unit Shutdown(bool unlinkFromParent = true)
+        {
+            return Unit.Default;
+        }
+    }
+}
