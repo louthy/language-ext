1eaa342d2a0b8f5d1eb4c2f10ba1801104de1061
Paul Louth <plouth@meddbase.com>
2015-06-26 02:44:20 +0100

    Correct comments

--- LanguageExt.Core/Prelude_Curry.cs
+++ LanguageExt.Core/Prelude_Curry.cs
@@ -49,7 +49,7 @@ namespace LanguageExt
             (T1 a) => (T2 b) => (T3 c) => (T4 d) => (T5 e) => f(a, b, c, d, e);
 
         /// <summary>
-        /// Curry the function 'f' provided.
+        /// Curry the function 'func' provided.
         /// You can then partially apply by calling: 
         /// 
         ///     var curried = curry(f);
@@ -61,7 +61,7 @@ namespace LanguageExt
 
 
         /// <summary>
-        /// Curry the function 'f' provided.
+        /// Curry the function 'func' provided.
         /// You can then partially apply by calling: 
         /// 
         ///     var curried = curry(f);
@@ -73,7 +73,7 @@ namespace LanguageExt
 
 
         /// <summary>
-        /// Curry the function 'f' provided.
+        /// Curry the function 'func' provided.
         /// You can then partially apply by calling: 
         /// 
         ///     var curried = curry(f);
@@ -84,22 +84,22 @@ namespace LanguageExt
             (T1 a) => (T2 b) => (T3 c) => (T4 d) => (T5 e) => (T6 f) => (T7 g) => (T8 h) => func(a, b, c, d, e, f, g, h);
 
         /// <summary>
-        /// Curry the function 'f' provided.
+        /// Curry the function 'func' provided.
         /// You can then partially apply by calling: 
         /// 
         ///     var curried = curry(f);
-        ///     var r = curried(a)(b)(c)(d)(e)(f)(g)(h)(h)
+        ///     var r = curried(a)(b)(c)(d)(e)(f)(g)(h)(i)
         /// 
         /// </summary>
         public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, R>>>>>>>>> curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, R> func) =>
             (T1 a) => (T2 b) => (T3 c) => (T4 d) => (T5 e) => (T6 f) => (T7 g) => (T8 h) => (T9 i) => func(a, b, c, d, e, f, g, h, i);
 
         /// <summary>
-        /// Curry the function 'f' provided.
+        /// Curry the function 'func' provided.
         /// You can then partially apply by calling: 
         /// 
         ///     var curried = curry(f);
-        ///     var r = curried(a)(b)(c)(d)(e)(f)(g)(h)(h)
+        ///     var r = curried(a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
         /// 
         /// </summary>
         public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, R>>>>>>>>>> curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R> func) =>
