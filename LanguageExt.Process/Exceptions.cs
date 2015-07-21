a1065b05713469ff4cfaa71c8b2382e1d691d0bb
Paul Louth <plouth@meddbase.com>
2015-07-21 12:54:46 +0100

    Remove un-needed TimeoutException that can easily clash with other TimeoutExceptions.

--- LanguageExt.Process/Exceptions.cs
+++ LanguageExt.Process/Exceptions.cs
@@ -94,26 +94,4 @@ namespace LanguageExt
         {
         }
     }
-
-    [Serializable]
-    public class TimeoutException : Exception
-    {
-        public TimeoutException()
-            :
-            base("The mailbox operation timed out.")
-        {
-        }
-
-        public TimeoutException(string message) : base(message)
-        {
-        }
-
-        public TimeoutException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected TimeoutException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-    }
 }
