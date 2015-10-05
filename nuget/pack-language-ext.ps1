68096c23e613acdcf586ea66840e61c4267703be
Paul Louth <p.louth@gmail.com>
2015-10-05 20:43:20 +0100

    Update pack-language-ext.ps1

--- nuget/pack-language-ext.ps1
+++ nuget/pack-language-ext.ps1
@@ -3,9 +3,9 @@ $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\nuget\LanguageExt.nuspec) 
+$content = (Get-Content .\nuget\LanguageExt.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\nuget\LanguageExt.compiled.nuspec
+$content | Out-File .\nuget\LanguageExt.compiled.nuspec
 
-& $root\NuGet\NuGet.exe pack $root\nuget\LanguageExt.compiled.nuspec
+& $root\NuGet\NuGet.exe pack .\nuget\LanguageExt.compiled.nuspec
