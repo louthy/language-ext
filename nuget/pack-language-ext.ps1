bbe74aad5692c12c02b5ccbeb01af0901ab2638f
Paul Louth <p.louth@gmail.com>
2015-10-05 20:24:53 +0100

    Create pack-language-ext.ps1

---
+++ nuget/pack-language-ext.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\lib\LanguageExt.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\nuget\LanguageExt.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\LanguageExt.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\nuget\LanguageExt.compiled.nuspec
