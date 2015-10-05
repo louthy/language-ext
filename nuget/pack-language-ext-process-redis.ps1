ba11e8b161ffcf619f1d83aa29ab57b5a458e0d5
Paul Louth <p.louth@gmail.com>
2015-10-05 20:44:43 +0100

    Update pack-language-ext-process-redis.ps1

--- nuget/pack-language-ext-process-redis.ps1
+++ nuget/pack-language-ext-process-redis.ps1
@@ -1,12 +1,11 @@
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\lib\LanguageExt.Process.Redis.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile(".\lib\LanguageExt.Process.Redis.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\nuget\LanguageExt.Process.Redis.nuspec) 
+$content = (Get-Content .\nuget\LanguageExt.Process.Redis.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\nuget\LanguageExt.Process.Redis.compiled.nuspec
+$content | Out-File .\nuget\LanguageExt.Process.Redis.compiled.nuspec
 
-& $root\NuGet\NuGet.exe pack $root\nuget\LanguageExt.Process.Redis.compiled.nuspec
+& Nuget pack .\nuget\LanguageExt.Process.Redis.compiled.nuspec
