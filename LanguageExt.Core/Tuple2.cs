306f01a7e7f60b423f2f216f118f7342d1b1c9ef
Paul Louth <p.louth@gmail.com>
2015-10-05 20:14:44 -0700

    Tuple bifunctor and bifoldable

--- LanguageExt.Core/Tuple2.cs
+++ LanguageExt.Core/Tuple2.cs
@@ -83,29 +83,58 @@ public static class __Tuple2
         self.Item1 + self.Item2;
 
     /// <summary>
-    /// Map tuple to R
+    /// Map to R
     /// </summary>
     public static R Map<T1, T2, R>(this Tuple<T1, T2> self, Func<T1, T2, R> map) =>
         map(self.Item1, self.Item2);
 
     /// <summary>
-    /// Map tuple to tuple
+    /// Map to tuple
     /// </summary>
     public static Tuple<R1, R2> Map<T1, T2, R1, R2>(this Tuple<T1, T2> self, Func<Tuple<T1, T2>, Tuple<R1, R2>> map) =>
         map(self);
 
     /// <summary>
-    /// Map tuple to tuple
+    /// Bi-map to tuple
+    /// </summary>
+    public static Tuple<R1, R2> Map<T1, T2, R1, R2>(this Tuple<T1, T2> self, Func<T1, R1> firstMap, Func<T2, R2> secondMap) =>
+        Tuple(firstMap(self.Item1),secondMap(self.Item2));
+
+    /// <summary>
+    /// Map to tuple
     /// </summary>
     public static Tuple<R1, R2> Select<T1, T2, R1, R2>(this Tuple<T1, T2> self, Func<Tuple<T1, T2>, Tuple<R1, R2>> map) =>
         map(self);
 
     /// <summary>
-    /// Tuple iterate
+    /// Iterate
     /// </summary>
     public static Unit Iter<T1, T2>(this Tuple<T1, T2> self, Action<T1, T2> func)
     {
         func(self.Item1, self.Item2);
         return Unit.Default;
     }
+
+    /// <summary>
+    /// Iterate
+    /// </summary>
+    public static Unit Iter<T1, T2>(this Tuple<T1, T2> self, Action<T1> first, Action<T2> second)
+    {
+        first(self.Item1);
+        second(self.Item2);
+        return Unit.Default;
+    }
+    
+    /// <summary>
+    /// Fold
+    /// </summary>
+    public static S Fold<T1, T2, S>(this Tuple<T1, T2> self, S state, Func<S,T1,T2,S> fold) =>
+        fold(state,self.Item1,self.Item2);
+    
+    /// <summary>
+    /// Bi-fold
+    /// </summary>
+    public static S Fold<T1, T2, S>(this Tuple<T1, T2> self, S state, Func<S,T1,S> firstFold, Func<S,T2,S> secondFold) =>
+        secondFold(firstFold(state,self.Item1),self.Item2);
+
 }
