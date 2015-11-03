4f92363fcb2ef3434c38a1cacba10d5cab04f71d
will_mooar <will.mooar@gmail.com>
2015-11-03 21:08:28 +1300

    Added some comments

--- LanguageExt.Core/NullableExt.cs
+++ LanguageExt.Core/NullableExt.cs
@@ -5,11 +5,23 @@ using static LanguageExt.Prelude;
 
 public static class __NullableExt
 {
+    /// <summary>
+    /// Convert NullableT to OptionT
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="self">Value to convert</param>
+    /// <returns>OptionT with Some or None, depending on HasValue</returns>
     public static Option<T> ToOption<T>(this T? self) where T : struct =>
         self.HasValue
             ? Some(self.Value)
             : None;
 
+    /// <summary>
+    /// Convert NullableT to IEnumerableT (0..1 entries)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="self">Value to convert</param>
+    /// <returns>Zero or One enumerable values, depending on HasValue</returns>
     public static IEnumerable<T> AsEnumerable<T>(this T? self) where T : struct
     {
         if (self.HasValue)
--- LanguageExt.Core/TryOutExt.cs
+++ LanguageExt.Core/TryOutExt.cs
@@ -4,6 +4,14 @@ using static LanguageExt.Prelude;
 
 public static class __TryOutExt
 {
+    /// <summary>
+    /// Get a value out of a dictionary as Some, otherwise None.
+    /// </summary>
+    /// <typeparam name="K">Key type</typeparam>
+    /// <typeparam name="V">Value type</typeparam>
+    /// <param name="self">Dictionary</param>
+    /// <param name="key">Key</param>
+    /// <returns>OptionT filled Some(value) or None</returns>
     public static Option<V> TryGetValue<K, V>(this IDictionary<K, V> self, K key)
     {
         V value;
@@ -11,6 +19,15 @@ public static class __TryOutExt
             ? Some(value)
             : None;
     }
+
+    /// <summary>
+    /// Get a value out of a dictionary as Some, otherwise None.
+    /// </summary>
+    /// <typeparam name="K">Key type</typeparam>
+    /// <typeparam name="V">Value type</typeparam>
+    /// <param name="self">Dictionary</param>
+    /// <param name="key">Key</param>
+    /// <returns>OptionT filled Some(value) or None</returns>
     public static Option<V> TryGetValue<K, V>(this IReadOnlyDictionary<K, V> self, K key)
     {
         V value;
