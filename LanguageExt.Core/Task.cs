27d76ef9120657fdee08b811e57afbb83391e399
paul louth <paul_louth@hotmail.com>
2016-03-31 02:00:49 +0100

    Async idea

---
+++ LanguageExt.Core/Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LanguageExt
+{
+    public delegate Task<T> Async<T>();
+
+    public static class __AsyncExt
+    {
+        public static Async<U> Select<T, U>(this Async<T> self, Func<T, U> map) =>
+            () => 
+                Observable.FromAsync(() => self())
+                          .Select(map)
+                          .ToTask();
+
+        public static Async<T> Where<T>(this Async<T> self, Func<T, bool> pred) =>
+            () =>
+                Observable.FromAsync(() => self())
+                          .Where(pred)
+                          .ToTask();
+
+        public static Async<V> SelectMany<T, U, V>(
+            this Async<T> self,
+            Func<T, Async<U>> bind,
+            Func<T, U, V> project
+            ) =>
+            () =>
+                Observable.FromAsync(() => self())
+                          .SelectMany(
+                                t => Observable.FromAsync(() => bind(t)()),
+                                project)
+                          .ToTask();
+
+        public static Async<T> Async<T>(this Func<T> f) => () => new Task<T>(f);
+        public static Async<T> Async<T>(this Task<T> t) => () => t;
+
+    }
+}
---
+++ LanguageExt.Tests/AsyncTests.cs
@@ -0,0 +1,56 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExtTests
+{
+    public class AsyncTests
+    {
+        [Fact]
+        public void AsyncLINQTest1()
+        {
+            var computation = from x in Action1().Async()
+                              from y in Action2().Async()
+                              from z in Action3().Async()
+                              from e in Action4().Async()
+                              select x + y + z + e;
+
+            var res1 = computation().Result;
+            var res2 = computation().Result;
+        }
+
+        public async Task<int> Action1()
+        {
+            await Task.Delay(100);
+            return 100;
+        }
+
+        public async Task<int> Action2()
+        {
+            await Task.Delay(200);
+            return 200;
+        }
+
+        public async Task<int> Action3()
+        {
+            await Task.Delay(300);
+            return 300;
+        }
+
+        public async Task<int> Action4()
+        {
+            await Task.Delay(400);
+
+            throw new Exception("Bleruehr");
+
+            return 400;
+        }
+
+    }
+}
