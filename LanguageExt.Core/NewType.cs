d10edb415d8cc140e0236d8eff424a45fd448e51
paul louth <paul_louth@hotmail.com>
2016-06-08 17:21:46 +0100

    NewType<T> - inspired by Haskell's 'newtype' keyword. Derive type from this one to get: Equatable, Comparable, Appendable, Subtractable, Multiplicable, Divisible strongly typed values.

---
+++ LanguageExt.Core/NewType.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
+using System.Reflection;
+
+namespace LanguageExt
+{
+    public interface INewType
+    {
+    }
+
+    /// <summary>
+    /// NewType - inspired by Haskell's 'newtype' keyword.
+    /// https://wiki.haskell.org/Newtype
+    /// Derive type from this one to get: Equatable, Comparable, Appendable, Subtractable, 
+    /// Multiplicable, Divisible strongly typed values.  For example:
+    ///     Metres : NewType<double>
+    ///     Hours : NewType<double>
+    /// </summary>
+    public class NewType<T> : 
+        IEquatable<NewType<T>>, 
+        IComparable<NewType<T>>, 
+        IAppendable<NewType<T>>,
+        ISubtractable<NewType<T>>,
+        IMultiplicable<NewType<T>>,
+        IDivisible<NewType<T>>,
+        INewType
+    {
+        public readonly T Value;
+
+        public NewType(T value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(NewType<T> other) =>
+            !Object.ReferenceEquals(other, null) &&
+            this.GetType() == other.GetType()
+                ? Comparer<T>.Default.Compare(Value, other.Value)
+                : -1;
+
+        public bool Equals(NewType<T> other) =>
+            !Object.ReferenceEquals(other, null) &&
+            this.GetType() == other.GetType() &&
+            Value.Equals(other.Value);
+
+        public override bool Equals(object obj) =>
+            !Object.ReferenceEquals(obj, null) &&
+            obj is NewType<T> &&
+            Equals((NewType<T>)obj);
+
+        public override int GetHashCode() =>
+            Value == null ? 0 : Value.GetHashCode();
+
+        public static bool operator ==(NewType<T> lhs, NewType<T> rhs) =>
+            lhs.Equals(rhs);
+
+        public static bool operator !=(NewType<T> lhs, NewType<T> rhs) =>
+            !lhs.Equals(rhs);
+
+        public static bool operator >(NewType<T> lhs, NewType<T> rhs) =>
+            !ReferenceEquals(lhs, null) &&
+            !ReferenceEquals(rhs, null) &&
+             lhs.GetType() == rhs.GetType()
+                ? lhs.CompareTo(rhs) > 0
+                : failwith<bool>("Mismatched NewTypes used with '>'");
+
+        public static bool operator >=(NewType<T> lhs, NewType<T> rhs) =>
+            !ReferenceEquals(lhs, null) &&
+            !ReferenceEquals(rhs, null) &&
+            lhs.GetType() == rhs.GetType()
+                ? lhs.CompareTo(rhs) >= 0
+                : failwith<bool>("Mismatched NewTypes used with '>='");
+
+        public static bool operator <(NewType<T> lhs, NewType<T> rhs) =>
+            !ReferenceEquals(lhs, null) &&
+            !ReferenceEquals(rhs, null) &&
+            lhs.GetType() == rhs.GetType()
+                ? lhs.CompareTo(rhs) < 0
+                : failwith<bool>("Mismatched NewTypes in used with '>'");
+
+        public static bool operator <=(NewType<T> lhs, NewType<T> rhs) =>
+            !ReferenceEquals(lhs, null) &&
+            !ReferenceEquals(rhs, null) &&
+            lhs.GetType() == rhs.GetType()
+                ? lhs.CompareTo(rhs) <= 0
+                : failwith<bool>("Mismatched NewTypes in used with '<='");
+
+        public static NewType<T> operator +(NewType<T> lhs, NewType<T> rhs) =>
+            lhs.Append(rhs);
+
+        public static NewType<T> operator -(NewType<T> lhs, NewType<T> rhs) =>
+            lhs.Subtract(rhs);
+
+        public static NewType<T> operator /(NewType<T> lhs, NewType<T> rhs) =>
+            lhs.Divide(rhs);
+
+        public static NewType<T> operator *(NewType<T> lhs, NewType<T> rhs) =>
+            lhs.Multiply(rhs);
+
+        public NewType<T> Map(Func<T, T> map) =>
+            Select(map);
+
+        public NewType<T> Select(Func<T, T> map) =>
+            (NewType<T>)NewType.Construct(GetType(), map(Value));
+
+        public NewType<T> SelectMany(
+            Func<T, NewType<T>> bind,
+            Func<T, T, T> project
+            )
+        {
+            var ures = bind(Value);
+            if (GetType() != ures.GetType()) throw new Exception("LINQ statement with mismatched NewTypes");
+            return (NewType<T>)NewType.Construct(GetType(), project(Value, ures.Value));
+        }
+
+        public Unit Iter(Action<T> f)
+        {
+            f(Value);
+            return unit;
+        }
+
+        public NewType<T> Append(NewType<T> rhs) =>
+            this.GetType() == rhs.GetType()
+                ? (NewType<T>)NewType.Construct(GetType(), TypeDesc.Append(Value, rhs.Value, TypeDesc<T>.Default))
+                : failwith<NewType<T>>("Mismatched NewTypes in append/add");
+
+        public NewType<T> Subtract(NewType<T> rhs) =>
+            this.GetType() == rhs.GetType()
+                ? (NewType<T>)NewType.Construct(GetType(), TypeDesc.Subtract(Value, rhs.Value, TypeDesc<T>.Default))
+                : failwith<NewType<T>>("Mismatched NewTypes in subtract");
+
+        public NewType<T> Divide(NewType<T> rhs) =>
+            this.GetType() == rhs.GetType()
+                ? (NewType<T>)NewType.Construct(GetType(), TypeDesc.Divide(Value, rhs.Value, TypeDesc<T>.Default))
+                : failwith<NewType<T>>("Mismatched NewTypes in divide");
+
+        public NewType<T> Multiply(NewType<T> rhs) =>
+            this.GetType() == rhs.GetType()
+                ? (NewType<T>)NewType.Construct(GetType(), TypeDesc.Multiply(Value, rhs.Value, TypeDesc<T>.Default))
+                : failwith<NewType<T>>("Mismatched NewTypes in multiply");
+
+        public NT Cast<NT>() where NT : NewType<T> =>
+            (NT)this;
+
+        public override string ToString() =>
+            $"{GetType().Name}({Value})";
+    }
+
+    public static class NewType
+    {
+        static Map<string, ConstructorInfo> constructors = Map.empty<string, ConstructorInfo>();
+
+        private static ConstructorInfo GetCtor(Type newType)
+        {
+            if (newType.Name == "NewType") throw new ArgumentException("Only use NewType.Contruct to build construct types derived from NewType<T>");
+            var ctors = (from c in newType.GetConstructors()
+                         where c.GetParameters().Length == 1
+                         select c)
+                        .ToArray();
+
+            if (ctors.Length > 1) throw new ArgumentException($"{newType.FullName} has more than one constructor with 1 parameter");
+
+            var ctor = ctors.First();
+            constructors = constructors.AddOrUpdate(newType.FullName,ctor);
+            return ctor;
+        }
+
+        public static object Construct(Type newTypeT, object arg) =>
+            constructors.Find(newTypeT.FullName).IfNone(GetCtor(newTypeT)).Invoke(new object[] { arg });
+
+        public static NewTypeT Construct<NewTypeT, T>(T arg) where NewTypeT : NewType<T> =>
+            (NewTypeT)constructors.Find(typeof(NewTypeT).FullName).IfNone(GetCtor(typeof(NewTypeT))).Invoke(new object[] { arg });
+    }
+
+}
---
+++ LanguageExt.Tests/NewTypeTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace LanguageExtTests
+{
+    public class Metres : NewType<int>
+    {
+        public Metres(int value) : base(value) { }
+    }
+
+    public class Hours : NewType<int>
+    {
+        public Hours(int value) : base(value) { }
+    }
+
+    public class NewTypeTests
+    {
+        [Fact]
+        public void EqTest1()
+        {
+            var m1 = new Metres(1);
+            var m2 = new Metres(1);
+            var m3 = new Metres(2);
+
+            var h1 = new Hours(1);
+            var h2 = new Hours(1);
+            var h3 = new Hours(2);
+
+            Assert.True(m1 == m2);
+            Assert.False(m1 == m3);
+            //var r3 = m1 == h1;    // won't compile
+        }
+
+        [Fact]
+        public void OrdTest1()
+        {
+            var m1 = new Metres(1);
+            var m2 = new Metres(2);
+
+            var h1 = new Hours(1);
+            var h2 = new Hours(1);
+
+            Assert.True(m1 < m2);
+            Assert.True(m2 > m1);
+            Assert.True(m1 <= m2);
+            Assert.True(m2 >= m1);
+
+            // Mixing types
+            Assert.Throws<Exception>(() => h1 > m2);
+            Assert.Throws<Exception>(() => h1 < m2);
+            Assert.Throws<Exception>(() => h1 >= m2);
+            Assert.Throws<Exception>(() => h1 >= m2);
+        }
+
+        [Fact]
+        public void LinqTest()
+        {
+            var m1 = new Metres(1);
+            var m2 = new Metres(2);
+
+            var res = from x in m1
+                      from y in m2
+                      select x + y;
+
+            Assert.True(res.Value == 3);
+            Assert.True(res is Metres);
+
+            var h1 = new Hours(1);
+
+            // Mixing types
+            Assert.Throws<Exception>(() =>
+                res = from x in m1
+                      from y in h1
+                      select x + y
+                  );
+        }
+
+        [Fact]
+        public void ArithmeticTest()
+        {
+            var m1 = new Metres(8);
+            var m2 = new Metres(2);
+
+            var r1 = m1 + m2;
+            var r2 = m1 - m2;
+            var r3 = m1 / m2;
+            var r4 = m1 * m2;
+
+            Assert.True(r1.Value == 10);
+            Assert.True(r2.Value == 6);
+            Assert.True(r3.Value == 4);
+            Assert.True(r4.Value == 16);
+
+            var h1 = new Hours(1);
+
+            // Mixing types
+            Assert.Throws<Exception>(() => m1 + h1);
+            Assert.Throws<Exception>(() => m1 - h1);
+            Assert.Throws<Exception>(() => m1 / h1);
+            Assert.Throws<Exception>(() => m1 * h1);
+        }
+    }
+}
