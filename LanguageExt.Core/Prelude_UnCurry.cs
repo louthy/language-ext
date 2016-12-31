22f613eb6a536ce628ddee7bc0bf28d85326c831
leandromoh <leandromoh@hotmail.com>
2016-12-31 12:44:28 -0200

    renamed functions from unCurry to uncurry

--- LanguageExt.Core/Prelude_UnCurry.cs
+++ LanguageExt.Core/Prelude_UnCurry.cs
@@ -9,7 +9,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, TResult> unCurry<T1, T2, TResult>(this Func<T1, Func<T2, TResult>> function)
+        public static Func<T1, T2, TResult> uncurry<T1, T2, TResult>(this Func<T1, Func<T2, TResult>> function)
         {
             return (arg1, arg2) => function(arg1)(arg2);
         }
@@ -18,7 +18,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, TResult> unCurry<T1, T2, T3, TResult>(this Func<T1, Func<T2, Func<T3, TResult>>> function)
+        public static Func<T1, T2, T3, TResult> uncurry<T1, T2, T3, TResult>(this Func<T1, Func<T2, Func<T3, TResult>>> function)
         {
             return (arg1, arg2, arg3) => function(arg1)(arg2)(arg3);
         }
@@ -27,7 +27,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, TResult> unCurry<T1, T2, T3, T4, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> function)
+        public static Func<T1, T2, T3, T4, TResult> uncurry<T1, T2, T3, T4, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> function)
         {
             return (arg1, arg2, arg3, arg4) => function(arg1)(arg2)(arg3)(arg4);
         }
@@ -36,7 +36,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, TResult> unCurry<T1, T2, T3, T4, T5, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, TResult> uncurry<T1, T2, T3, T4, T5, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5) => function(arg1)(arg2)(arg3)(arg4)(arg5);
         }
@@ -45,7 +45,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, TResult> unCurry<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, TResult> uncurry<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6);
         }
@@ -54,7 +54,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7);
         }
@@ -63,7 +63,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8);
         }
@@ -72,7 +72,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, TResult>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, TResult>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9);
         }
@@ -81,7 +81,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, TResult>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, TResult>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10);
         }
@@ -90,7 +90,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, TResult>>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, TResult>>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10)(arg11);
         }
@@ -99,7 +99,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, TResult>>>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, TResult>>>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10)(arg11)(arg12);
         }
@@ -108,7 +108,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, TResult>>>>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, TResult>>>>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10)(arg11)(arg12)(arg13);
         }
@@ -117,7 +117,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, TResult>>>>>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, TResult>>>>>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10)(arg11)(arg12)(arg13)(arg14);
         }
@@ -126,7 +126,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, TResult>>>>>>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, TResult>>>>>>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10)(arg11)(arg12)(arg13)(arg14)(arg15);
         }
@@ -135,7 +135,7 @@ namespace LanguageExt
         /// Transforms a curried function into a function that takes multiple arguments
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> unCurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, Func<T16, TResult>>>>>>>>>>>>>>>> function)
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> uncurry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, Func<T16, TResult>>>>>>>>>>>>>>>> function)
         {
             return (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16) => function(arg1)(arg2)(arg3)(arg4)(arg5)(arg6)(arg7)(arg8)(arg9)(arg10)(arg11)(arg12)(arg13)(arg14)(arg15)(arg16);
         }
