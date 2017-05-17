46aec11324e5d03102e1dd95c8f84e663bc6d9bc
Paul Louth <p.louth@gmail.com>
2017-05-17 13:21:29 +0100

    Breaking change: Flipped Compose and BackCompose  - The extension methods felt like they were back-to-front, Compose and BackCompose are now reverse so that: a.Compose(b) works rather than b.Compose(a).  It allows for easier chaining of functions.

--- LanguageExt.Core/Compose.cs
+++ LanguageExt.Core/Compose.cs
@@ -14,7 +14,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>v => b(a(v))</returns>
         [Pure]
-        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T2, T3> b, Func<T1, T2> a) =>
+        public static Func<T1, T3> BackCompose<T1, T2, T3>(this Func<T2, T3> b, Func<T1, T2> a) =>
             v => b(a(v));
 
         /// <summary>
@@ -22,7 +22,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>() => b(a())</returns>
         [Pure]
-        public static Func<T2> Compose<T1, T2>(this Func<T1, T2> b, Func<T1> a) =>
+        public static Func<T2> BackCompose<T1, T2>(this Func<T1, T2> b, Func<T1> a) =>
             () => b(a());
 
         /// <summary>
@@ -30,7 +30,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>v => b(a(v))</returns>
         [Pure]
-        public static Action<T1> Compose<T1, T2>(this Action<T2> b, Func<T1, T2> a)
+        public static Action<T1> BackCompose<T1, T2>(this Action<T2> b, Func<T1, T2> a)
             => v => b(a(v));
 
         /// <summary>
@@ -38,7 +38,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>() => b(a())</returns>
         [Pure]
-        public static Action Compose<T>(this Action<T> b, Func<T> a)
+        public static Action BackCompose<T>(this Action<T> b, Func<T> a)
             => () => b(a());
 
         /// <summary>
@@ -46,7 +46,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>v => b(a(v))</returns>
         [Pure]
-        public static Func<T1, T3> BackCompose<T1, T2, T3>(this Func<T1, T2> a, Func<T2, T3> b) =>
+        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> a, Func<T2, T3> b) =>
             v => b(a(v));
 
         /// <summary>
@@ -54,7 +54,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>() => b(a())</returns>
         [Pure]
-        public static Func<T2> BackCompose<T1, T2>(this Func<T1> a, Func<T1, T2> b) =>
+        public static Func<T2> Compose<T1, T2>(this Func<T1> a, Func<T1, T2> b) =>
             () => b(a());
 
         /// <summary>
@@ -62,7 +62,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>v => b(a(v))</returns>
         [Pure]
-        public static Action<T1> BackCompose<T1, T2>(this Func<T1, T2> a, Action<T2> b)
+        public static Action<T1> Compose<T1, T2>(this Func<T1, T2> a, Action<T2> b)
             => v => b(a(v));
 
         /// <summary>
@@ -70,7 +70,7 @@ namespace LanguageExt
         /// </summary>
         /// <returns>() => b(a())</returns>
         [Pure]
-        public static Action BackCompose<T>(this Func<T> a, Action<T> b)
+        public static Action Compose<T>(this Func<T> a, Action<T> b)
             => () => b(a());
     }
 }
