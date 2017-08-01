3be853197445626be690fc4dba9f5de976d49472
Paul Louth <p.louth@gmail.com>
2017-08-01 21:40:25 +0100

    Update RecordsNetCore example

--- Samples/RecordsNetCore/Program.cs
+++ Samples/RecordsNetCore/Program.cs
@@ -8,67 +8,29 @@ namespace RecordsNetCore
     {
         static void Main(string[] args)
         {
-            GitterTests.TestSerial();
-        }
-    }
+            var opt1 = Maybe.Just(100);
+            var opt2 = Maybe.Just(100);
+            var optN = Maybe.Nothing<int>();
 
-    public class TenantId : NewType<TenantId, int> { public TenantId(int value) : base(value) { } };
-    public class UserId : NewType<UserId, int> { public UserId(int value) : base(value) { } };
-    public class Instant : NewType<Instant, int> { public Instant(int value) : base(value) { } };
+            var res1 = opt1.Match(
+                           Just: x => x * 2,
+                           Nothing: () => 0);
 
-    public class Collector : Record<Collector>
-    {
-        public CollectorId Id { get; }
-        public string Name { get; }
-        public TenantId CurrentTenant { get; }
-        public UserId AssignedBy { get; }
-        public Instant InstantAssigned { get; }
-        public Collector(Some<CollectorId> id, Some<string> name, Some<TenantId> tenant, Some<UserId> assignedBy, Instant dateAssigned)
-        {
-            Id = id;
-            Name = name;
-            CurrentTenant = tenant;
-            AssignedBy = assignedBy;
-            InstantAssigned = dateAssigned;
-        }
+            var res2 = optN.Match(
+                           Just: x => x * 2,
+                           Nothing: () => 0);
 
-        Collector(SerializationInfo info, StreamingContext ctx) : base(info, ctx) { }
-    }
+            var sign = opt1.Equals(optN) ? "==" : "!=";
+            Console.WriteLine($"{opt1} {sign} {optN}");
 
-    public class GitterTests
-    {
-        public static void TestSerial()
-        {
-            var x = new Collector(new CollectorId(1), "nick", TenantId.New(2), UserId.New(3), Instant.New(4));
-            var y = new Collector(new CollectorId(1), "nick", TenantId.New(2), UserId.New(3), Instant.New(4));
-            var z = new Collector(new CollectorId(4), "nick", TenantId.New(2), UserId.New(3), Instant.New(4));
+            sign = opt1.Equals(opt2) ? "==" : "!=";
+            Console.WriteLine($"{opt1} {sign} {opt2}");
 
-            var z1 = x == y;
-            var z2 = x.Equals(y);
-            var z3 = x.Equals((object)y);
-
-            var nz1 = x == z;
-            var nz2 = x.Equals(z);
-            var nz3 = x.Equals((object)z);
-
-        }
-    }
-
-    public abstract class NonZeroIntegerId
-    {
-        protected NonZeroIntegerId(int value)
-        {
-            Value = value;
-        }
-        public int Value { get; }
-
-        public static implicit operator int(NonZeroIntegerId id) => id.Value;
-    }
-
-    public class CollectorId : NonZeroIntegerId
-    {
-        public CollectorId(int id) : base(id)
-        {
+            Console.WriteLine($"opt1: {opt1}");
+            Console.WriteLine($"optN: {optN}");
+            Console.WriteLine($"res1: {res1}");
+            Console.WriteLine($"res2: {res2}");
+            Console.ReadKey();
         }
     }
 }
