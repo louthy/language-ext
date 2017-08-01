b9f7ff94ced231cde9dd355b10f691d7056cb531
Paul Louth <p.louth@gmail.com>
2017-08-01 19:11:02 +0100

    Discriminated union example

--- Samples/Records/Maybe.cs
+++ Samples/Records/Maybe.cs
@@ -5,21 +5,32 @@ using System.Text;
 namespace Records
 {
     /// <summary>
-    /// Maybe type
+    /// Maybe discriminated union example
     /// </summary>
     public interface Maybe<A>
     { }
 
+    /// <summary>
+    /// Just case
+    /// </summary>
+    /// <typeparam name="A"></typeparam>
     public sealed class Just<A> : Record<Just<A>>, Maybe<A>
     {
         public readonly A Value;
         public Just(A value) => Value = value;
     }
 
+    /// <summary>
+    /// Nothing case
+    /// </summary>
+    /// <typeparam name="A"></typeparam>
     public sealed class Nothing<A> : Record<Nothing<A>>, Maybe<A>
     {
     }
 
+    /// <summary>
+    /// Extensions and construction
+    /// </summary>
     public static class Maybe
     {
         public static Maybe<A> Just<A>(A value) => 
@@ -29,8 +40,9 @@ namespace Records
             new Nothing<A>();
 
         public static R Match<A, R>(this Maybe<A> ma, Func<A, R> Just, Func<R> Nothing) =>
-            ma is Just<A> just ? Just(just.Value)
+            ma is null         ? Nothing()
           : ma is Nothing<A> _ ? Nothing()
+          : ma is Just<A> just ? Just(just.Value)
           : throw new InvalidOperationException();
     }
 }
