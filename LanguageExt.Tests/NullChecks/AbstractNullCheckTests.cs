5c19be741605023904a98fcdb7d14e9b4e938cc8
Tyson Williams <tyson.d.w@gmail.com>
2017-12-24 07:33:07 -0600

    added rewrites of existing null check tests to acheive one assert per test

---
+++ LanguageExt.Tests/NullChecks/AbstractNullCheckTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace LanguageExt.Tests.NullChecks
+{
+    public abstract class AbstractNullCheckTests
+    {
+        protected abstract bool ExpectedWhenNull { get; }
+        protected abstract bool NullCheck<T>(T value);
+
+        protected bool ExpectedWhenNotNull => !ExpectedWhenNull;
+
+        [Fact]
+        public void NullCheck_NullString_AsExpectedWhenNull()
+        {
+            string value = null;
+
+            var actual = NullCheck(value);
+
+            Assert.Equal(ExpectedWhenNull, actual);
+        }
+
+        [Fact]
+        public void NullCheck_HelloString_AsExpectedWhenNotNull()
+        {
+            string value = "hello";
+
+            var actual = NullCheck(value);
+
+            Assert.Equal(ExpectedWhenNotNull, actual);
+        }
+
+        [Fact]
+        public void NullCheck_ZeroInt_AsExpectedWhenNotNull()
+        {
+            int value = 0;
+
+            var actual = NullCheck(value);
+
+            Assert.Equal(ExpectedWhenNotNull, actual);
+        }
+
+    }
+}
---
+++ LanguageExt.Tests/NullChecks/IsNullExtensionTests.cs
@@ -0,0 +1,8 @@
+﻿namespace LanguageExt.Tests.NullChecks
+{
+    public class IsNullExtensionTests : AbstractNullCheckTests
+    {
+        protected override bool ExpectedWhenNull => true;
+        protected override bool NullCheck<T>(T value) => value.IsNull();
+    }
+}
