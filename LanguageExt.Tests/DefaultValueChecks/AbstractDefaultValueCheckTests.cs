cd4db0cfd969e1c39fc44acd050c3ce2553ec56d
Tyson Williams <tyson.d.w@gmail.com>
2017-12-24 23:06:04 -0600

    added more tests for the functions that do default value checks

--- LanguageExt.Tests/DefaultValueChecks/AbstractDefaultValueCheckTests.cs
+++ LanguageExt.Tests/DefaultValueChecks/AbstractDefaultValueCheckTests.cs
@@ -10,6 +10,22 @@ namespace LanguageExt.Tests.DefaultValueChecks
         private bool ExpectedWhenNotDefaultValue => !ExpectedWhenDefaultValue;
 
         [Fact]
+        public void DefaultValueCheck_DefaultValueObject_AsExpectedWhenDefaultValue()
+        {
+            object value = null;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_DefaultConstructorObject_AsExpectedWhenNotDefaultValue()
+        {
+            object value = new object();
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenNotDefaultValue, actual);
+        }
+
+        [Fact]
         public void DefaultValueCheck_DefaultValueString_AsExpectedWhenDefaultValue()
         {
             string value = null;
@@ -39,6 +55,43 @@ namespace LanguageExt.Tests.DefaultValueChecks
             int value = 100;
             var actual = DefaultValueCheck(value);
             Assert.Equal(ExpectedWhenNotDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_DefaultValueNullableByte_AsExpectedWhenDefaultValue()
+        {
+            byte? value = null;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_ZeroNullableByte_AsExpectedWhenNotDefaultValue()
+        {
+            byte? value = 0;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenNotDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_DefaultConstructorEnum_AsExpectedWhenDefaultValue()
+        {
+            FooEnum value = new FooEnum();
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_FirstEnumOptionWithValueOne_AsExpectedWhenNotDefaultValue()
+        {
+            FooEnum value = FooEnum.FooEnumState;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenNotDefaultValue, actual);
+        }
+
+        private enum FooEnum
+        {
+            FooEnumState = 1
         }
     }
 }
