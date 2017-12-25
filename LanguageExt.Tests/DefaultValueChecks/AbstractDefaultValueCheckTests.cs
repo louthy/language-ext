c9d46811608f91ed64eac9c39a2b6a45f29dc5ea
Tyson Williams <tyson.d.w@gmail.com>
2017-12-24 22:42:34 -0600

    added rewrites of existing default value check tests to acheive one assert per test

---
+++ LanguageExt.Tests/DefaultValueChecks/AbstractDefaultValueCheckTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+
+namespace LanguageExt.Tests.DefaultValueChecks
+{
+    public abstract class AbstractDefaultValueCheckTests
+    {
+        protected abstract bool ExpectedWhenDefaultValue { get; }
+        protected abstract bool DefaultValueCheck<T>(T value);
+
+        private bool ExpectedWhenNotDefaultValue => !ExpectedWhenDefaultValue;
+
+        [Fact]
+        public void DefaultValueCheck_DefaultValueString_AsExpectedWhenDefaultValue()
+        {
+            string value = null;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_HelloString_AsExpectedWhenNotDefaultValue()
+        {
+            string value = "hello";
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenNotDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_DefaultValueInt_AsExpectedWhenDefaultValue()
+        {
+            int value = 0;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenDefaultValue, actual);
+        }
+
+        [Fact]
+        public void DefaultValueCheck_NonDefaultValueInt_AsExpectedWhenNotDefaultValue()
+        {
+            int value = 100;
+            var actual = DefaultValueCheck(value);
+            Assert.Equal(ExpectedWhenNotDefaultValue, actual);
+        }
+    }
+}
