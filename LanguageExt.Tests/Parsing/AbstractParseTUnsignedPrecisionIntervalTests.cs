f1590f4773f7ac5d6668eef785b63b135a43533c
Tyson Williams <tyson.d.w@gmail.com>
2017-12-28 08:13:33 -0600

    added parseT tests for many types T

---
+++ LanguageExt.Tests/Parsing/AbstractParseTUnsignedPrecisionIntervalTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace LanguageExt.Tests.Parsing
+{
+    public abstract class AbstractParseTUnsignedPrecisionIntervalTests<T>
+        : AbstractParseTPrecisionIntervalTests<T>
+        where T : struct
+    {
+        protected abstract T PositiveOne { get; }
+        protected abstract T PositiveTwo { get; }
+
+        [Fact]
+        public void ParseT_ValidStringFromPositiveOne_SomePositiveOne() =>
+            ParseT_ValidStringFromGiven_SomeAsGiven(PositiveOne);
+
+        [Fact]
+        public void ParseT_ValidStringFromPositiveTwo_SomePositiveTwo() =>
+            ParseT_ValidStringFromGiven_SomeAsGiven(PositiveTwo);
+    }
+}
