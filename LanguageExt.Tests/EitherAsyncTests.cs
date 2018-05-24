7343eba56bb4b3849dedc9f744cd30169a1ff5eb
EUROPE\yetsalko <yevhenii.tsalko@microsoft.com>
2018-05-24 17:18:03 +0200

    Add test

--- LanguageExt.Tests/EitherAsyncTests.cs
+++ LanguageExt.Tests/EitherAsyncTests.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using static LanguageExt.Prelude;
 
 namespace LanguageExt.Tests
 {
-    class EitherAsyncTests
+    using Xunit;
+
+    public class EitherAsyncTests
     {
+        [Fact]
+        public async Task ToAsyncExtensionLeftTest()
+        {
+            var eitherAsync = Left<Exception, Unit>(new Exception()).AsTask().ToAsync();
+
+            await eitherAsync.Match(
+                v => Assert.True(false), // should never happen
+                e => Assert.True(true)
+            );
+        }
     }
 }
