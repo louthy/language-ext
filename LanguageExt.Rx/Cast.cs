9245d49e8c626239ac98fb69bef50c1bc8495c94
Paul Louth <p.louth@gmail.com>
2018-06-04 20:02:41 +0100

    fix cast issue

--- LanguageExt.Rx/Cast.cs
+++ LanguageExt.Rx/Cast.cs
@@ -3,11 +3,11 @@ namespace LanguageExt
 {
     internal static class CastExtensions
     {
-        public static A Cast<A>(this Option<A> ma) => (A)(object)ma;
-        public static A Cast<A>(this OptionUnsafe<A> ma) => (A)(object)ma;
-        public static L CastLeft<L, R>(this Either<L, R> ma) => (L)(object)ma;
-        public static R CastRight<L, R>(this Either<L, R> ma) => (R)(object)ma;
-        public static L CastLeft<L, R>(this EitherUnsafe<L, R> ma) => (L)(object)ma;
-        public static R CastRight<L, R>(this EitherUnsafe<L, R> ma) => (R)(object)ma;
+        public static A Cast<A>(this Option<A> ma) => (A)ma;
+        public static A Cast<A>(this OptionUnsafe<A> ma) => (A)ma;
+        public static L CastLeft<L, R>(this Either<L, R> ma) => (L)ma;
+        public static R CastRight<L, R>(this Either<L, R> ma) => (R)ma;
+        public static L CastLeft<L, R>(this EitherUnsafe<L, R> ma) => (L)ma;
+        public static R CastRight<L, R>(this EitherUnsafe<L, R> ma) => (R)ma;
     }
 }
