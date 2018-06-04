d4623a0885207c1f39f0d31c573222a4b227532e
Paul Louth <p.louth@gmail.com>
2018-06-04 19:00:23 +0100

    Update pack bat to build Rx

--- pack.bat
+++ pack.bat
@@ -2,3 +2,4 @@ dotnet restore
 dotnet pack LanguageExt.Core -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.FSharp -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.Parsec -c Release -o ../../artifacts/bin
+dotnet pack LanguageExt.Rx -c Release -o ../../artifacts/bin
