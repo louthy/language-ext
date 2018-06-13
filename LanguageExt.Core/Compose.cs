2998477be0b4a8e3bf2d0210e7007807ba11dcd0
Tyson Williams <tyson.d.w@gmail.com>
2018-06-13 16:47:30 -0500

    remove unused usings

--- LanguageExt.Core/Compose.cs
+++ LanguageExt.Core/Compose.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LanguageExt
 {
