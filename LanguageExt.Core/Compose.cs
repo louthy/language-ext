15e4404bb31c8d4e08b45024ac9f60ad7be8f66b
Tyson Williams <tyson.d.w@gmail.com>
2018-06-13 16:59:18 -0500

    changed parameter names to better distingish them from the new type parameter names to coming next

--- LanguageExt.Core/Compose.cs
+++ LanguageExt.Core/Compose.cs
@@ -8,65 +8,65 @@ namespace LanguageExt
         /// <summary>
         /// Function back composition
         /// </summary>
-        /// <returns>v => b(a(v))</returns>
+        /// <returns>v => g(f(v))</returns>
         [Pure]
-        public static Func<T1, T3> BackCompose<T1, T2, T3>(this Func<T2, T3> b, Func<T1, T2> a) =>
-            v => b(a(v));
+        public static Func<T1, T3> BackCompose<T1, T2, T3>(this Func<T2, T3> g, Func<T1, T2> f) =>
+            v => g(f(v));
 
         /// <summary>
         /// Function back composition
         /// </summary>
-        /// <returns>() => b(a())</returns>
+        /// <returns>() => g(f())</returns>
         [Pure]
-        public static Func<T2> BackCompose<T1, T2>(this Func<T1, T2> b, Func<T1> a) =>
-            () => b(a());
+        public static Func<T2> BackCompose<T1, T2>(this Func<T1, T2> g, Func<T1> f) =>
+            () => g(f());
 
         /// <summary>
         /// Action back composition
         /// </summary>
-        /// <returns>v => b(a(v))</returns>
+        /// <returns>v => g(f(v))</returns>
         [Pure]
-        public static Action<T1> BackCompose<T1, T2>(this Action<T2> b, Func<T1, T2> a)
-            => v => b(a(v));
+        public static Action<T1> BackCompose<T1, T2>(this Action<T2> g, Func<T1, T2> f)
+            => v => g(f(v));
 
         /// <summary>
         /// Action back composition
         /// </summary>
-        /// <returns>() => b(a())</returns>
+        /// <returns>() => g(f())</returns>
         [Pure]
-        public static Action BackCompose<T>(this Action<T> b, Func<T> a)
-            => () => b(a());
+        public static Action BackCompose<T>(this Action<T> g, Func<T> f)
+            => () => g(f());
 
         /// <summary>
         /// Function composition
         /// </summary>
-        /// <returns>v => b(a(v))</returns>
+        /// <returns>v => g(f(v))</returns>
         [Pure]
-        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> a, Func<T2, T3> b) =>
-            v => b(a(v));
+        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> f, Func<T2, T3> g) =>
+            v => g(f(v));
 
         /// <summary>
         /// Function composition
         /// </summary>
-        /// <returns>() => b(a())</returns>
+        /// <returns>() => g(f())</returns>
         [Pure]
-        public static Func<T2> Compose<T1, T2>(this Func<T1> a, Func<T1, T2> b) =>
-            () => b(a());
+        public static Func<T2> Compose<T1, T2>(this Func<T1> f, Func<T1, T2> g) =>
+            () => g(f());
 
         /// <summary>
         /// Action composition
         /// </summary>
-        /// <returns>v => b(a(v))</returns>
+        /// <returns>v => g(f(v))</returns>
         [Pure]
-        public static Action<T1> Compose<T1, T2>(this Func<T1, T2> a, Action<T2> b)
-            => v => b(a(v));
+        public static Action<T1> Compose<T1, T2>(this Func<T1, T2> f, Action<T2> g)
+            => v => g(f(v));
 
         /// <summary>
         /// Action composition
         /// </summary>
-        /// <returns>() => b(a())</returns>
+        /// <returns>() => g(f())</returns>
         [Pure]
-        public static Action Compose<T>(this Func<T> a, Action<T> b)
-            => () => b(a());
+        public static Action Compose<T>(this Func<T> f, Action<T> g)
+            => () => g(f());
     }
 }
