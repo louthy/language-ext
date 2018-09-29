a8e5b714651c6a1e707b1bb7705e1ff18e442155
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2018-09-29 10:09:52 +0900

    Added failing test that Seq properly disposes the IEnumerator

--- LanguageExt.Tests/Seq.Enumerable.Tests.cs
+++ LanguageExt.Tests/Seq.Enumerable.Tests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using static LanguageExt.Prelude;
 
@@ -246,5 +248,45 @@ namespace LanguageExt.Tests
             Assert.True(fa1);
             Assert.False(fa2);
         }
+
+        [Fact]
+        public void SeqEnumeratorDoesntDisposeEnumerator()
+        {
+            var myEnumerable = new MyEnumerable<int>();
+            myEnumerable.ToSeq().Apply(Enumerable.FirstOrDefault);
+            Assert.True(myEnumerable.Enumerator.WasDisposed);
+        }
+
+        private class MyEnumerable<T> : IEnumerable<T>
+        {
+            public MyEnumerable()
+            {
+                this.Enumerator = new MyEnumerator<T>();
+            }
+
+            public MyEnumerator<T> Enumerator { get; }
+            public IEnumerator<T> GetEnumerator() => this.Enumerator;
+            IEnumerator IEnumerable.GetEnumerator() => this.Enumerator;
+        }
+
+        private class MyEnumerator<T> : IEnumerator<T>
+        {
+            public MyEnumerator()
+            {
+                this.WasDisposed = false;
+            }
+            public bool WasDisposed { get; private set; }
+
+            public bool MoveNext() => true;
+
+            public void Reset() { }
+
+            public T Current => default;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() => WasDisposed = true;
+        }
+
     }
 }
