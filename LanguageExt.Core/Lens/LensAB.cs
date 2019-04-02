0f71aea7be3600b563b143b009d31103dde3ed07
Paul Louth <p.louth@gmail.com>
2019-04-03 00:04:10 +0100

    First pass at lenses

---
+++ LanguageExt.Core/Lens/LensAB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt
+{
+    /// <summary>
+    /// Primitive lens type for creating well-behaved bidirectional transformations
+    /// </summary>
+    public struct Lens<A, B>
+    {
+        public readonly Func<A, B> Get;
+        public readonly Func<B, Func<A, A>> SetF;
+
+        Lens(Func<A, B> get, Func<B, Func<A, A>> set)
+        {
+            Get = get;
+            SetF = set;
+        }
+
+        public A Set(B value, A cont) =>
+            SetF(value)(cont);
+
+        public static Lens<A, B> New(Func<A, B> Get, Func<B, Func<A, A>> Set) =>
+            new Lens<A, B>(Get, Set);
+
+        public Func<A, A> Update(Func<B, B> f)
+        {
+            var self = this;
+            return a => self.Set(f(self.Get(a)), a);
+        }
+    }
+}
--- LanguageExt.Tests/IssuesTests.cs
+++ LanguageExt.Tests/IssuesTests.cs
@@ -500,4 +500,45 @@ namespace Issues
         }
 
     }
+
+    // https://stackoverflow.com/questions/54609459/languageext-eitherasyn-with-aggegrate-bind-with-validation
+    public class StackOverflow_54609459
+    {
+        public class Error { }
+        public class HostResponse { }
+        public class Response { }
+
+        public class Command {
+
+            public readonly string Name;
+
+            static Either<Error, Func<string, EitherAsync<Error, R>>> GetCommand<R>(
+                Map<string, Func<string, EitherAsync<Error, R>>> commandMap, 
+                Command hostCommand) =>
+                     commandMap.Find(hostCommand.Name)
+                               .ToEither(new Error());
+
+            internal static EitherAsync<Error, R> ExecuteCommand<R>(
+                Func<string, EitherAsync<Error, R>> command,
+                Command cmd) =>
+                    command(cmd.Name);
+
+            static Either<Error, Unit> Validate<R>(
+                Map<string, Func<string, EitherAsync<Error, R>>> commandMap, 
+                Command hostCommand) =>
+                    commandMap.Find(hostCommand.Name)
+                              .Map(_ => unit)
+                              .ToEither(new Error());
+
+            public static EitherAsync<Error, Seq<R>> ExecuteAllAsync<R>(
+                Map<string, Func<string, EitherAsync<Error, R>>> commandMap,
+                Seq<Command> hostCommands) =>
+                    hostCommands.Map(cmd =>
+                        from _ in Command.Validate(commandMap, cmd).ToAsync()
+                        from f in Command.GetCommand<R>(commandMap, cmd).ToAsync()
+                        from r in Command.ExecuteCommand(f, cmd)
+                        select r)
+                       .Sequence();
+        }
+    }
 }
