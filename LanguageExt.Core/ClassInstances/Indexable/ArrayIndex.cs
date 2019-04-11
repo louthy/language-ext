a808406797c1a9b2e5d04fa2747cad69d8012f94
Paul Louth <p.louth@gmail.com>
2019-04-11 20:10:57 +0100

    Indexable type-class and class-instances

---
+++ LanguageExt.Core/ClassInstances/Indexable/ArrayIndex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using LanguageExt.Attributes;
+using LanguageExt.TypeClasses;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.ClassInstances
+{
+    public struct ArrayIndex<A> : Indexable<A[], int, A>
+    {
+        public A Get(A[] ma, int key) =>
+            TryGet(ma, key).IfNone(() => throw new IndexOutOfRangeException(nameof(key)));
+
+        [Pure]
+        public Option<A> TryGet(A[] ma, int key) =>
+            ma == null || key < 0 || key >= ma.Length
+                ? None
+                : Some(ma[key]);
+    }
+}
---
+++ LanguageExt.Core/ClassInstances/Indexable/MapIndex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using LanguageExt.Attributes;
+using LanguageExt.TypeClasses;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.ClassInstances
+{
+    public struct MapIndex<K, A> : Indexable<Map<K, A>, K, A>
+    {
+        public A Get(Map<K, A> ma, K key) =>
+            TryGet(ma, key).IfNone(() => throw new IndexOutOfRangeException(nameof(key)));
+
+        [Pure]
+        public Option<A> TryGet(Map<K, A> ma, K key) =>
+            ma.Find(key);
+    }
+}
