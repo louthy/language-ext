c9df58a4897ac90a604f1cb9dedcd685e0fcde40
Paul Louth <p.louth@gmail.com>
2019-04-11 21:48:45 +0100

    Add CodeGen project to pack list

--- pack.bat
+++ pack.bat
@@ -3,3 +3,4 @@ dotnet pack LanguageExt.Core -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.FSharp -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.Parsec -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.Rx -c Release -o ../../artifacts/bin
+dotnet pack LanguageExt.CodeGen -c Release -o ../../artifacts/bin
