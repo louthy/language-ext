f342660057f4ec98f05b01e1f2a29cb90bb4b5f9
Tyson Williams <tyson.d.w@gmail.com>
2019-04-12 09:04:30 -0500

    optimized static loading of Record<> #549

--- LanguageExt.Core/DataTypes/Record/RecordType.cs
+++ LanguageExt.Core/DataTypes/Record/RecordType.cs
@@ -11,60 +11,119 @@ namespace LanguageExt
     /// <typeparam name="A">Type to provide methods for</typeparam>
     public static class RecordType<A>
     {
-        static RecordType()
-        {
-            bool includeBase = typeof(A).CustomAttributes
-                                        .Exists(a => a.AttributeType.Name == nameof(IgnoreBaseAttribute))
-                                        .Apply(not);
-
-            Hash = IL.GetHashCode<A>(includeBase);
-            Equality = IL.Equals<A>(includeBase);
-            EqualityTyped = IL.EqualsTyped<A>(includeBase);
-            Compare = IL.Compare<A>(includeBase);
-            ToString = IL.ToString<A>(includeBase);
-            SetObjectData = IL.SetObjectData<A>(includeBase);
-            GetObjectData = IL.GetObjectData<A>(includeBase);
-        }
-
         /// <summary>
         /// General hash code function for record types
         /// </summary>
-        public static readonly Func<A, int> Hash;
+        public static Func<A, int> Hash => RecordTypeHash<A>.Hash;
 
         /// <summary>
         /// General equality function for record types
         /// </summary>
-        public static readonly Func<A, object, bool> Equality;
+        public static Func<A, object, bool> Equality => RecordTypeEquality<A>.Equality;
 
         /// <summary>
         /// General typed equality function for record types
         /// </summary>
-        public static readonly Func<A, A, bool> EqualityTyped;
+        public static Func<A, A, bool> EqualityTyped => RecordTypeEqualityTyped<A>.EqualityTyped;
 
         /// <summary>
         /// General typed comparison function for record types
         /// </summary>
-        public static readonly Func<A, A, int> Compare;
+        public static Func<A, A, int> Compare => RecordTypeCompare<A>.Compare;
 
         /// <summary>
         /// General ToString function
         /// </summary>
-        public new static readonly Func<A, string> ToString;
+        public static new Func<A, string> ToString => RecordTypeToString<A>.ToString;
 
         /// <summary>
         /// De-serialise an A
         /// </summary>
-        public static readonly Action<A, SerializationInfo> SetObjectData;
+        public static Action<A, SerializationInfo> SetObjectData => RecordTypeSetObjectData<A>.SetObjectData;
 
         /// <summary>
         /// Serialise an A
         /// </summary>
-        public static readonly Action<A, SerializationInfo> GetObjectData;
+        public static Action<A, SerializationInfo> GetObjectData => RecordTypeGetObjectData<A>.GetObjectData;
 
         [Obsolete("Don't use Equals - use either RecordType<A>.Equality or RecordType<A>.EqualityTyped")]
-        public new static bool Equals(object objA, object objB)
+        public static new bool Equals(object objA, object objB)
         {
             throw new InvalidOperationException("Don't use Equals - use either RecordType<A>.Equality or RecordType<A>.EqualityTyped");
         }
+    }
+
+    internal static class RecordTypeIncludeBase<A>
+    {
+        static RecordTypeIncludeBase()
+        {
+            IncludeBase = typeof(A).CustomAttributes
+                                   .Exists(a => a.AttributeType.Name == nameof(IgnoreBaseAttribute))
+                                   .Apply(not);
+        }
+        internal static readonly bool IncludeBase;
+    }
+
+    internal static class RecordTypeHash<A>
+    {
+        static RecordTypeHash()
+        {
+            Hash = IL.GetHashCode<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static readonly Func<A, int> Hash;
+    }
+
+    internal static class RecordTypeEquality<A>
+    {
+        static RecordTypeEquality()
+        {
+            Equality = IL.Equals<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static readonly Func<A, object, bool> Equality;
+    }
+
+    internal static class RecordTypeEqualityTyped<A>
+    {
+        static RecordTypeEqualityTyped()
+        {
+            EqualityTyped = IL.EqualsTyped<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static readonly Func<A, A, bool> EqualityTyped;
+    }
+
+    internal static class RecordTypeCompare<A>
+    {
+        static RecordTypeCompare()
+        {
+            Compare = IL.Compare<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static readonly Func<A, A, int> Compare;
+    }
+
+    internal static class RecordTypeToString<A>
+    {
+        static RecordTypeToString()
+        {
+            ToString = IL.ToString<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static new readonly Func<A, string> ToString;
+    }
+
+    internal static class RecordTypeSetObjectData<A>
+    {
+        static RecordTypeSetObjectData()
+        {
+            SetObjectData = IL.SetObjectData<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static readonly Action<A, SerializationInfo> SetObjectData;
+    }
+
+    internal static class RecordTypeGetObjectData<A>
+    {
+        static RecordTypeGetObjectData()
+        {
+            GetObjectData = IL.GetObjectData<A>(RecordTypeIncludeBase<A>.IncludeBase);
+        }
+        internal static readonly Action<A, SerializationInfo> GetObjectData;
     }
 }
