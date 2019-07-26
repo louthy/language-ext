6b2ed6f0c9afcda2acdbbd5d1649f57c88c6c2bf
James Saucier <jamessaucier@gmail.com>
2019-07-25 21:30:59 -0700

    remove and sort usings

--- Samples/Contoso/Contoso.Infrastructure/Data/Configurations/OfficeAssignmentConfiguration.cs
+++ Samples/Contoso/Contoso.Infrastructure/Data/Configurations/OfficeAssignmentConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Contoso.Core.Domain;
+﻿using Contoso.Core.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
