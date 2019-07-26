a948bf205b7662c1045d3d88ad6cf3d09f0cb6d3
James Saucier <jamessaucier@gmail.com>
2019-07-25 21:39:08 -0700

    add dbContext to startup

--- Samples/Contoso/Contoso.Web/Startup.cs
+++ Samples/Contoso/Contoso.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contoso.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contoso.Web
 {
@@ -26,6 +28,8 @@ namespace Contoso.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ContosoDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ContosoDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
