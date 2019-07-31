92eaf9d1890d345790731fd464e5df83056568aa
James Saucier <jamessaucier@gmail.com>
2019-07-30 20:03:22 -0700

    remove some usings

--- Samples/Contoso/Contoso.Web/Controllers/InstructorsController.cs
+++ Samples/Contoso/Contoso.Web/Controllers/InstructorsController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Contoso.Application.Instructors.Commands;
 using Contoso.Application.Instructors.Queries;
 using Contoso.Web.Extensions;
