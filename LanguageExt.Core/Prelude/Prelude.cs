f57927a9c680d0ad38f64465abbc53e5c0a20a6d
Paul Louth <p.louth@gmail.com>
2019-08-07 23:47:24 +0100

    Reader generator

--- LanguageExt.Core/Prelude/Prelude.cs
+++ LanguageExt.Core/Prelude/Prelude.cs
@@ -352,19 +352,5 @@ namespace LanguageExt
         [Pure]
         public static string toString<A>(A value) =>
             value?.ToString() ?? "";
-
-        /// <summary>
-        /// Flip the arguments in a two argument function
-        /// </summary>
-        [Pure]
-        public static Func<B, A, C> flip<A, B, C>(Func<A, B, C> f) =>
-            (b, a) => f(a, b);
-
-        /// <summary>
-        /// Flip the arguments in a two argument function
-        /// </summary>
-        [Pure]
-        public static Func<B, Func<A, C>> flip<A, B, C>(Func<A, Func<B, C>> f) =>
-            b => a => f(a)(b);
     }
 }
