6b3eb72bbf15e465ba79e79c86eca60cdba3407c
Paul Louth <p.louth@gmail.com>
2019-08-16 10:47:39 +0100

    WindowIter

--- LanguageExt.Core/DataTypes/Task/Task.Extensions.cs
+++ LanguageExt.Core/DataTypes/Task/Task.Extensions.cs
@@ -265,5 +265,60 @@ namespace LanguageExt
             await source;
             return unit;
         }
+
+        /// <summary>
+        /// Tasks a lazy sequence of tasks and iterates them in a 'measured way'.  A default window size of
+        /// `Environment.ProcessorCount / 2` tasks is used, which means there are `Environment.ProcessorCount / 2`
+        /// 'await streams'.  An await stream essentially awaits one task from the sequence, and on completion
+        /// goes and gets the next task from the lazy sequence and awaits that too.  This continues until the end
+        /// of the lazy sequence, or forever for infinite streams.
+        /// </summary>
+        public static Task<Unit> WindowIter<A>(this IEnumerable<Task<A>> ma, Action<A> f) =>
+            WindowIter(ma, Math.Max(1, Environment.ProcessorCount >> 1), f);
+
+        /// <summary>
+        /// Tasks a lazy sequence of tasks and iterates them in a 'measured way'.  A default window size of
+        /// `windowSize` tasks is used, which means there are `windowSize` 'await streams'.  An await stream 
+        /// essentially awaits one task from the sequence, and on completion goes and gets the next task from 
+        /// the lazy sequence and awaits that too.  This continues until the end of the lazy sequence, or forever 
+        /// for infinite streams.
+        /// </summary>
+        public static async Task<Unit> WindowIter<A>(this IEnumerable<Task<A>> ma, int windowSize, Action<A> f)
+        {
+            object sync = new object();
+            var iter = ma.GetEnumerator();
+
+            (bool Success, Task<A> Task) GetNext()
+            {
+                lock (sync)
+                {
+                    return iter.MoveNext()
+                        ? (true, iter.Current)
+                        : default;
+                }
+            }
+
+            var tasks = new List<Task<Unit>>();
+            for (var i = 0; i < windowSize; i++)
+            {
+                var (s, outerTask) = GetNext();
+                if (!s) break;
+
+                tasks.Add(outerTask.Iter(async oa =>
+                {
+                    f(oa);
+
+                    while (true)
+                    {
+                        var next = GetNext();
+                        if (!next.Success) return;
+                        var a = await next.Task;
+                        f(a);
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
+            return unit;
+        }
     }
 }
