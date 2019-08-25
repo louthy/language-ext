441707f4f75092176dd10d7ed7c7e64746ebea99
James Saucier <jamessaucier@gmail.com>
2019-08-24 22:22:51 -0700

    fix matching for tasks

--- Samples/Contoso/Contoso.Web/Extensions/EitherToActionResult.cs
+++ Samples/Contoso/Contoso.Web/Extensions/EitherToActionResult.cs
@@ -7,15 +7,20 @@ namespace Contoso.Web.Extensions
 {
     public static class EitherToActionResult
     {
-        public static IActionResult ToActionResult<L, R>(this Either<L, R> either) =>
+        public static Task<IActionResult> ToActionResult<L, R>(this Task<Either<L, R>> either) =>
+            either.Map(Match);
+
+        public static Task<IActionResult> ToActionResult(this Task<Either<Error, Task>> either) =>
+            either.Bind(Match);
+
+        private static IActionResult Match<L, R>(this Either<L, R> either) =>
             either.Match<IActionResult>(
                 Left: l => new BadRequestObjectResult(l),
                 Right: r => new OkObjectResult(r));
 
-        public static Task<IActionResult> ToActionResult<L, R>(this Task<Either<L, R>> either) =>
-            either.Map(ToActionResult);
-
-        public static Task<IActionResult> ToActionResult(this Task<Either<Error, Task>> either) =>
-            either.Map(ToActionResult);
+        private async static Task<IActionResult> Match(Either<Error, Task> either) =>
+            await either.MatchAsync<IActionResult>(
+                RightAsync: async t => { await t; return new OkResult(); },
+                Left: e => new BadRequestObjectResult(e));
     }
 }
