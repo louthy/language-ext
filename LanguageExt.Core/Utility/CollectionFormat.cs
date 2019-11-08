b41a1b831acf3e1827913c394443419f42799da3
Paul Louth <p.louth@gmail.com>
2019-11-08 12:18:49 +0000

    Collection ToShortString helper function

---
+++ LanguageExt.Core/Utility/CollectionFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LanguageExt
+{
+    public static class CollectionFormat
+    {
+        public static string ToShortString<A>(IEnumerable<A> ma, int length = -1, string separator = ", ")
+        {
+            var items = ma.Take(50).ToList();
+
+            return items.Count < length || length == -1
+                ? $"{String.Join(separator, items)}"
+                : $"{String.Join(separator, items)}...";
+        }
+    }
+}
