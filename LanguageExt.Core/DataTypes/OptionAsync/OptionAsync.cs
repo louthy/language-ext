01bec9f47e962b88292c30083b776be0ab860bff
Paul Louth <p.louth@gmail.com>
2020-02-07 21:01:06 +0000

    Move from netstandard2.0 to netstandard2.1
    IAsyncEnumerable for EitherAsync and OptionAsync

--- LanguageExt.Core/DataTypes/OptionAsync/OptionAsync.cs
+++ LanguageExt.Core/DataTypes/OptionAsync/OptionAsync.cs
@@ -8,6 +8,7 @@ using System.Diagnostics.Contracts;
 using LanguageExt.ClassInstances;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace LanguageExt
 {
@@ -29,6 +30,9 @@ namespace LanguageExt
     /// </summary>
     /// <typeparam name="A">Bound value</typeparam>
     public struct OptionAsync<A> :
+#if NETCORE
+        IAsyncEnumerable<A>,
+#endif
         IOptionalAsync
     {
         internal readonly Task<(bool IsSome, A Value)> data;
@@ -1236,5 +1240,20 @@ namespace LanguageExt
         [Pure]
         public OptionAsync<Func<B, Func<C, D>>> ParMap<B, C, D>(Func<A, B, C, D> func) =>
             Map(curry(func));
+
+#if NETCORE
+        /// <summary>
+        /// Enumerate asynchronously
+        /// </summary>
+        [Pure]
+        public async IAsyncEnumerator<A> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            var (isSome, value) = await Data;
+            if(isSome)
+            {
+                yield return value;
+            }
+        }
+#endif
     }
 }
