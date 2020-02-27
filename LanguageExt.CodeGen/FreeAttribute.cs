9d813fe267add848f742aec143a28bf667043e46
Paul Louth <p.louth@gmail.com>
2020-02-27 14:39:14 +0000

    Free monad code-gen

---
+++ LanguageExt.CodeGen/FreeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using CodeGeneration.Roslyn;
+using LanguageExt.CodeGen;
+
+namespace LanguageExt
+{
+    /// <summary>
+    /// Union attribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    [CodeGenerationAttribute(typeof(FreeGenerator))]
+    [Conditional("CodeGeneration")]
+    public class FreeAttribute : Attribute
+    {
+    }
+}
