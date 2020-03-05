d022d52e4ef24007381ebf3cb184572d807d2e48
Paul Louth <p.louth@gmail.com>
2020-03-05 19:04:04 +0000

    EitherUnsafe Traverse unit tests

---
+++ LanguageExt.Tests/Transformer/Traverse/EitherUnsafe/Sync/TryEither.cs
@@ -0,0 +1,82 @@
+using System;
+using Xunit;
+using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests.Transformer.Traverse.EitherUnsafe.Sync
+{
+    public class TryEitherUnsafe
+    {
+        [Fact]
+        public void WithErrorLeft_FailIsLeft()
+        {
+            var ma = Try<EitherUnsafe<Error, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<Error, Try<int>>(Error.New("fail"));
+
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void WithExceptionLeft_FailIsLeft()
+        {
+            var ma = Try<EitherUnsafe<Exception, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<Exception, Try<int>>(new Exception("fail"));
+
+            var mr = mb.LeftToSeq().Head().Message == mc.LeftToSeq().Head().Message;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void WithStringLeft_FailIsLeft()
+        {
+            var ma = Try<EitherUnsafe<string, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<string, Try<int>>("fail");
+            
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }        
+        
+        [Fact]
+        public void WithUnknownLeft_ResultIsBottom()
+        {
+            var ma = Try<EitherUnsafe<bool, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+
+            Assert.True(mb.IsBottom);
+        }        
+        
+        [Fact]
+        public void SuccLeftIsLeft()
+        {
+            var ma = Try<EitherUnsafe<Error, int>>(Left(Error.New("alt")));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<Error, Try<int>>(Error.New("alt"));
+
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void SuccRightIsRight()
+        {
+            var ma = Try<EitherUnsafe<Error, int>>(Right(1234));
+            var mb = ma.Sequence();
+            var mc = RightUnsafe<Error, Try<int>>(Try(1234));
+
+            var mr = (from tb in mb
+                      from tc in mc
+                      select tb.Try().Equals(tc.Try())).IfLeftUnsafe(false);
+            
+            Assert.True(mr);
+        }
+    }
+}
---
+++ LanguageExt.Tests/Transformer/Traverse/EitherUnsafe/Sync/TryOptionEither.cs
@@ -0,0 +1,96 @@
+using System;
+using Xunit;
+using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests.Transformer.Traverse.EitherUnsafe.Sync
+{
+    public class TryOptionEitherUnsafe
+    {
+        [Fact]
+        public void WithErrorLeft_FailIsLeft()
+        {
+            var ma = TryOption<EitherUnsafe<Error, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<Error, TryOption<int>>(Error.New("fail"));
+
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void WithExceptionLeft_FailIsLeft()
+        {
+            var ma = TryOption<EitherUnsafe<Exception, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<Exception, TryOption<int>>(new Exception("fail"));
+
+            var mr = mb.LeftToSeq().Head().Message == mc.LeftToSeq().Head().Message;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void WithStringLeft_FailIsLeft()
+        {
+            var ma = TryOption<EitherUnsafe<string, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<string, TryOption<int>>("fail");
+            
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }        
+        
+        [Fact]
+        public void WithUnknownLeft_ResultIsBottom()
+        {
+            var ma = TryOption<EitherUnsafe<bool, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+
+            Assert.True(mb.IsBottom);
+        }        
+        
+        [Fact]
+        public void SuccLeftIsLeft()
+        {
+            var ma = TryOption<EitherUnsafe<Error, int>>(Left(Error.New("alt")));
+            var mb = ma.Sequence();
+            var mc = LeftUnsafe<Error, TryOption<int>>(Error.New("alt"));
+
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void SuccRightIsRight()
+        {
+            var ma = TryOption<EitherUnsafe<Error, int>>(Right(1234));
+            var mb = ma.Sequence();
+            var mc = RightUnsafe<Error, TryOption<int>>(TryOption(1234));
+
+            var mr = (from tb in mb
+                      from tc in mc
+                      select tb.Try().Equals(tc.Try())).IfLeftUnsafe(false);
+            
+            Assert.True(mr);
+        }
+
+        [Fact]
+        public void NoneIsRightNone()
+        {
+            var ma = TryOption<EitherUnsafe<Error, int>>(None);
+            var mb = ma.Sequence();
+            var mc = RightUnsafe<Error, TryOption<int>>(TryOption<int>(None));
+
+            var mr = (from tb in mb
+                      from tc in mc
+                      select tb.Try().Equals(tc.Try())).IfLeftUnsafe(false);
+            
+            Assert.True(mr);
+        }
+    }
+}
