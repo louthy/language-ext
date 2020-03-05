48d2e9d58ff6d068dee8acb47a7435dc328ef509
Paul Louth <p.louth@gmail.com>
2020-03-05 16:59:38 +0000

    TryOption and Validation with inner Either unit tests

---
+++ LanguageExt.Tests/Transformer/Traverse/Either/Sync/TryOptionEither.cs
@@ -0,0 +1,96 @@
+using System;
+using Xunit;
+using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests.Transformer.Traverse.Either.Sync
+{
+    public class TryOptionEither
+    {
+        [Fact]
+        public void WithErrorLeft_FailIsLeft()
+        {
+            var ma = TryOption<Either<Error, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = Left<Error, TryOption<int>>(Error.New("fail"));
+
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void WithExceptionLeft_FailIsLeft()
+        {
+            var ma = TryOption<Either<Exception, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = Left<Exception, TryOption<int>>(new Exception("fail"));
+
+            var mr = mb.LeftToSeq().Head().Message == mc.LeftToSeq().Head().Message;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void WithStringLeft_FailIsLeft()
+        {
+            var ma = TryOption<Either<string, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+            var mc = Left<string, TryOption<int>>("fail");
+            
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }        
+        
+        [Fact]
+        public void WithUnknownLeft_ResultIsBottom()
+        {
+            var ma = TryOption<Either<bool, int>>(new Exception("fail"));
+            var mb = ma.Sequence();
+
+            Assert.True(mb.IsBottom);
+        }        
+        
+        [Fact]
+        public void SuccLeftIsLeft()
+        {
+            var ma = TryOption<Either<Error, int>>(Left(Error.New("alt")));
+            var mb = ma.Sequence();
+            var mc = Left<Error, TryOption<int>>(Error.New("alt"));
+
+            var mr = mb == mc;
+            
+            Assert.True(mr);
+        }
+        
+        [Fact]
+        public void SuccRightIsRight()
+        {
+            var ma = TryOption<Either<Error, int>>(Right(1234));
+            var mb = ma.Sequence();
+            var mc = Right<Error, TryOption<int>>(TryOption(1234));
+
+            var mr = (from tb in mb
+                      from tc in mc
+                      select tb.Try().Equals(tc.Try())).IfLeft(false);
+            
+            Assert.True(mr);
+        }
+
+        [Fact]
+        public void NoneIsRightNone()
+        {
+            var ma = TryOption<Either<Error, int>>(None);
+            var mb = ma.Sequence();
+            var mc = Right<Error, TryOption<int>>(TryOption<int>(None));
+
+            var mr = (from tb in mb
+                      from tc in mc
+                      select tb.Try().Equals(tc.Try())).IfLeft(false);
+            
+            Assert.True(mr);
+        }
+    }
+}
