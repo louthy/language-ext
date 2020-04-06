280f71b359b3bd66b5c7d8807d912dd851341cbf
garyng <garyng.zhongbo@gmail.com>
2020-04-06 15:03:54 +0800

    Add samples for using codegen in netstandard library
    The reference of the generator need to be modified according to https://github.com/AArnott/CodeGeneration.Roslyn#customize-generator-reference

---
+++ Samples/CodeGen/Models/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LanguageExt;
+
+namespace Models
+{
+    [Union]
+    public interface Shape
+    {
+        Shape Rectangle(float width, float length);
+        Shape Circle(float radius);
+        Shape Prism(float width, float height);
+    }
+
+    public class UseShapeLibrary
+    {
+        public double GetArea(Shape shape)
+            => shape switch
+            {
+                Rectangle rec => rec.Length * rec.Width,
+                Circle circle => 2 * Math.PI * circle.Radius,
+                _ => throw new NotImplementedException()
+            };
+    }
+}
