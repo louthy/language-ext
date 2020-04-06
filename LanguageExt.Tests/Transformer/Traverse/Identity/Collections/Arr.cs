f730ea46292bd730cd8c9a74c224e241d4949ca4
James Saucier <james.saucier@lgm.ca>
2020-04-06 11:57:57 -0700

    Add identity collection traverse tests

---
+++ LanguageExt.Tests/Transformer/Traverse/Identity/Collections/Arr.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests.Transformer.Traverse.Identity.Collections
+{
+    public class ArrIdentity
+    {
+        [Fact]
+        public void EmptyArrayIsSuccess()
+        {
+            Arr<Identity<int>> ma = Empty;
+
+            var mb = ma.Traverse(identity);
+            
+            Assert.Equal(Id(Arr<int>.Empty), mb);
+        }
+
+        [Fact]
+        public void ArrOfIdentitiesIsIdentityOfArr()
+        {
+            var ma = Array(Id(1), Id(3), Id(5));
+
+            var mb = ma.Traverse(identity);
+
+            Assert.Equal(Id(Array(1, 3, 5)), mb);
+        }
+    }
+}
---
+++ LanguageExt.Tests/Transformer/Traverse/Identity/Collections/Lst.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests.Transformer.Traverse.Identity.Collections
+{
+    public class Lst
+    {
+        [Fact]
+        public void EmptyLstIsEmpty()
+        {
+            var ma = Lst<Identity<int>>.Empty;
+
+            var mb = ma.Traverse(identity);
+
+            Assert.Equal(Id(Lst<int>.Empty), mb);
+        }
+
+        [Fact]
+        public void LstOfIdentitiesIsIdentityOfLst()
+        {
+            var ma = List(Id(1), Id(3), Id(5));
+
+            var mb = ma.Traverse(identity);
+
+            Assert.Equal(Id(List(1, 3, 5)), mb);
+        }
+    }
+}
