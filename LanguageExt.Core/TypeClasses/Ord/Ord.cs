bf4a711c7be1472ea251ee8c7cf59e0a5a297eb6
Paul Louth <plouth@meddbase.com>
2020-05-06 11:06:20 +0100

    Remove Compare from Ord as it's now in IComparer

--- LanguageExt.Core/TypeClasses/Ord/Ord.cs
+++ LanguageExt.Core/TypeClasses/Ord/Ord.cs
@@ -6,19 +6,5 @@ namespace LanguageExt.TypeClasses
     [Typeclass("Ord*")]
     public interface Ord<A> : Eq<A>, System.Collections.Generic.IComparer<A>
     {
-        /// <summary>
-        /// Compare two values
-        /// </summary>
-        /// <param name="x">Left hand side of the compare operation</param>
-        /// <param name="y">Right hand side of the compare operation</param>
-        /// <returns>
-        /// if x greater than y : 1
-        /// 
-        /// if x less than y    : -1
-        /// 
-        /// if x equals y       : 0
-        /// </returns>
-        [Pure]
-        int Compare(A x, A y);
     }
 }
