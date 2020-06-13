38f29ad7f6873a225cd30d4532291cfbbe17f329
James Saucier <jamessaucier@gmail.com>
2020-06-12 20:25:03 -0700

    Add stck collections tests

---
+++ LanguageExt.Tests/Transformer/Traverse/Stck/Collections/Stck.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests.Transformer.Traverse.Stck.Collections
+{
+    public class Stck
+    {
+        [Fact]
+        public void EmptyEmptyIsEmptyEmpty()
+        {
+            Stck<Stck<int>> ma = Empty;
+            var mb = ma.Traverse(identity);
+            Stck<Stck<int>> mc = Empty;
+
+            Assert.Equal(mc, mb);
+        }
+
+        [Fact]
+        public void StckStckCrossProduct()
+        {
+            var ma = Stack(Stack(1, 2), Stack(3, 4));
+            var mb = ma.Traverse(identity);
+            var mc = Stack(
+                Stack(1, 3),
+                Stack(1, 4),
+                Stack(2, 3),
+                Stack(2, 4));
+
+            Assert.Equal(mc, mb);
+        }
+
+        [Fact]
+        public void StckOfMixedEmptiesIsEmpty()
+        {
+            var ma = Stack(Stck<int>.Empty, Stack(1, 2));
+            var mb = ma.Traverse(identity);
+            Stck<Stck<int>> mc = Empty;
+
+            Assert.Equal(mc, mb);
+        }
+
+        [Fact]
+        public void StckOfEmptiesIsEmpty()
+        {
+            var ma = Stack(Stack<int>(), Stack<int>());
+            var mb = ma.Traverse(identity);
+            var mc = Stck<Stck<int>>.Empty;
+
+            Assert.Equal(mc, mb);
+        }
+    }
+}
