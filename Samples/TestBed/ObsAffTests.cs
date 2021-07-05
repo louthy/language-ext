39a27cb37b45060adc8dc7a8df645c440007d04d
Paul Louth <plouth@meddbase.com>
2021-07-05 14:12:23 +0100

    Observable to Aff - fold test

--- Samples/TestBed/ObsAffTests.cs
+++ Samples/TestBed/ObsAffTests.cs
@@ -1,18 +1,8 @@
 using System;
 using LanguageExt;
-using LanguageExt.Common;
-using LanguageExt.Effects;
-using LanguageExt.Effects.Traits;
 using LanguageExt.Sys;
-using LanguageExt.Sys.Live;
-using LanguageExt.Sys.Traits;
-using LanguageExt.Sys.IO;
-using static LanguageExt.Prelude;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Reactive.Threading;
-using System.Reactive.Threading.Tasks;
-using System.Threading;
+using LanguageExt.Sys.Live;
 using System.Threading.Tasks;
 
 namespace TestBed
@@ -21,7 +11,7 @@ namespace TestBed
     {
         public static async Task Test()
         {
-            var obs = Observable.Interval(TimeSpan.FromSeconds(1), TaskPoolScheduler.Default);
+            var obs = Observable.Interval(TimeSpan.FromSeconds(1));
 
             var aff = obs.Fold<Runtime, int, long>(0, (s, x) => from n in DateTime<Runtime>.now
                                                                 from _ in Console<Runtime>.writeLine($"{s}: {n}")
