c95c3a15e2a6a743b287b749cf5dcca92cf64ddf
Paul Louth <plouth@meddbase.com>
2021-10-20 00:50:20 +0100

    Deployment script

--- pack.bat
+++ pack.bat
@@ -1,16 +1,23 @@
 I:
 
 echo building the docs
-cd I:\dev\best-form\lang-ext.cmd
+
+cd I:\dev\best-form\BestForm\bin\Release\net5.0
+bestform.exe "LanguageExt.Core" "I:\Dev\language-ext\LanguageExt.Core" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Parsec" "I:\Dev\language-ext\LanguageExt.Parsec" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.FSharp" "I:\Dev\language-ext\LanguageExt.FSharp" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Rx" "I:\Dev\language-ext\LanguageExt.Rx" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Sys" "I:\Dev\language-ext\LanguageExt.Sys" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
 
 echo committing them to git
 
-cd I:\dev\best-form\louthy.github.io
+cd I:\dev\louthy.github.io
 
 git add .
 git commit -m "Language-ext documentation update"
+git push
 
-cd I:\dev\best-form\language-ext
+cd I:\dev\language-ext
 
 echo building the artefacts
 
