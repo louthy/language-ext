8b38f5077b10686e0641ed3272111d97886a8838
Paul Louth <plouth@meddbase.com>
2021-10-20 00:39:41 +0100

    Language-ext documentation update

--- pack.bat
+++ pack.bat
@@ -1,3 +1,19 @@
+I:
+
+echo building the docs
+cd I:\dev\best-form\lang-ext.cmd
+
+echo committing them to git
+
+cd I:\dev\best-form\louthy.github.io
+
+git add .
+git commit -m "Language-ext documentation update"
+
+cd I:\dev\best-form\language-ext
+
+echo building the artefacts
+
 dotnet restore
 dotnet pack LanguageExt.Core -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.FSharp -c Release -o ../../artifacts/bin
@@ -5,3 +21,4 @@ dotnet pack LanguageExt.Parsec -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.Rx -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.Sys -c Release -o ../../artifacts/bin
 dotnet pack LanguageExt.CodeGen -c Release -o ../../artifacts/bin
+
