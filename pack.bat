d12c73ae54fba0b4487aa05b4b0458da13a47590
louthy <p.louth@gmail.com>
2022-03-07 14:45:33 +0000

    v4.0.4

--- pack.bat
+++ pack.bat
@@ -2,22 +2,22 @@ I:
 
 echo building the docs
 
-cd I:\dev\best-form\BestForm\bin\Release\net5.0
-bestform.exe "LanguageExt.Core" "I:\Dev\language-ext\LanguageExt.Core" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Parsec" "I:\Dev\language-ext\LanguageExt.Parsec" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.FSharp" "I:\Dev\language-ext\LanguageExt.FSharp" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Rx" "I:\Dev\language-ext\LanguageExt.Rx" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Sys" "I:\Dev\language-ext\LanguageExt.Sys" "I:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+cd Q:\dev\best-form\BestForm\bin\Release\net5.0
+bestform.exe "LanguageExt.Core" "Q:\Dev\language-ext\LanguageExt.Core" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Parsec" "Q:\Dev\language-ext\LanguageExt.Parsec" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.FSharp" "Q:\Dev\language-ext\LanguageExt.FSharp" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Rx" "Q:\Dev\language-ext\LanguageExt.Rx" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Sys" "Q:\Dev\language-ext\LanguageExt.Sys" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
 
 echo committing them to git
 
-cd I:\dev\louthy.github.io
+cd Q:\dev\louthy.github.io
 
 git add .
 git commit -m "Language-ext documentation update"
 git push
 
-cd I:\dev\language-ext
+cd Q:\dev\language-ext
 
 echo building the artefacts
 
