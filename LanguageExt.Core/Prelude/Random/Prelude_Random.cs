f39b1105cf76720911c8b7d6ed40a96f509ae25e
louthy <p.louth@gmail.com>
2022-06-15 10:33:21 +0100

    Fix for Prelude.random can throw an OverflowException
    https://github.com/louthy/language-ext/issues/1053
    
     - Removed the use of Math.Abs
     - Also, stopped the allocation of a heap array for the bytes read from the RandomNumberGenerator
     - This meant the lock can go also

--- LanguageExt.Core/Prelude/Random/Prelude_Random.cs
+++ LanguageExt.Core/Prelude/Random/Prelude_Random.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace LanguageExt
 {
     public static partial class Prelude
     {
-        readonly static RandomNumberGenerator rnd = RandomNumberGenerator.Create();
-        readonly static byte[] inttarget = new byte[4];
+        // There is no documentation that specifies whether the underlying RNG is thread-safe.
+        // It is assumed that the implementation is `RNGCryptoServiceProvider`, which under the
+        // hood calls `CryptGenRandom` from `advapi32` (i.e. a built-in Windows RNG).  There is
+        // no mention of thread-safety issues in any documentation, so we assume this must be 
+        // thread-safe.
+        //
+        // Documentation of `CrypGenRandom`
+        //
+        //    https://docs.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-cryptgenrandom
+        //
+        // There is some discussion here:
+        //
+        //    https://stackoverflow.com/questions/46147805/is-cryptgenrandom-thread-safe
+        //
+        static readonly RandomNumberGenerator rnd = RandomNumberGenerator.Create();
+        static readonly int wordTop = BitConverter.IsLittleEndian ? 3 : 0;
 
         /// <summary>
         /// Thread-safe cryptographically strong random number generator
@@ -15,11 +32,11 @@ namespace LanguageExt
         /// <returns>A non-negative random number, less than the value specified.</returns>
         public static int random(int max)
         {
-            lock (rnd)
-            {
-                rnd.GetBytes(inttarget);
-                return Math.Abs(BitConverter.ToInt32(inttarget, 0)) % max;
-            }
+            var bytes = ArrayPool<byte>.Shared.Rent(4);
+            rnd.GetBytes(bytes);
+            bytes[wordTop] &= 0x7f;
+            var value = BitConverter.ToInt32(bytes, 0) % max;
+            return value;
         }
 
         /// <summary>
@@ -31,12 +48,11 @@ namespace LanguageExt
         public static string randomBase64(int bytesCount)
         {
             if (bytesCount < 1) throw new ArgumentException($"The minimum value for {nameof(bytesCount)} is 1");
-
-            var bytes = new byte[bytesCount];
-            lock (rnd) {
-                rnd.GetBytes(bytes); 
-            }
-            return Convert.ToBase64String(bytes);
+            var bytes = ArrayPool<byte>.Shared.Rent(bytesCount);
+            rnd.GetBytes(bytes);
+            var r = Convert.ToBase64String(bytes);
+            ArrayPool<byte>.Shared.Return(bytes);
+            return r;
         }
     }
 }
