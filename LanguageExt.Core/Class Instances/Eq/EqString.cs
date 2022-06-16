29de44b89b8b3f1422ee9b50ec6e777871c6edad
louthy <p.louth@gmail.com>
2022-06-16 10:55:34 +0100

    Add invariant culture Eq
    https://github.com/louthy/language-ext/issues/1054

--- LanguageExt.Core/Class Instances/Eq/EqString.cs
+++ LanguageExt.Core/Class Instances/Eq/EqString.cs
@@ -10,7 +10,7 @@ namespace LanguageExt.ClassInstances
     /// </summary>
     public struct EqString : Eq<string>
     {
-        public static readonly EqString Inst = default(EqString);
+        public static readonly EqString Inst = default;
 
         /// <summary>
         /// Equality test
@@ -39,13 +39,83 @@ namespace LanguageExt.ClassInstances
         public Task<int> GetHashCodeAsync(string x) =>
             GetHashCode(x).AsTask();
     }
+    
+    /// <summary>
+    /// String equality (invariant culture)
+    /// </summary>
+    public struct EqStringInvariantCulture : Eq<string>
+    {
+        public static readonly EqStringInvariantCulture Inst = default;
+
+        /// <summary>
+        /// Equality test
+        /// </summary>
+        /// <param name="a">The left hand side of the equality operation</param>
+        /// <param name="b">The right hand side of the equality operation</param>
+        /// <returns>True if a and b are equal</returns>
+        [Pure]
+        public bool Equals(string a, string b) =>
+            StringComparer.InvariantCulture.Equals(a, b);
+
+        /// <summary>
+        /// Get hash code of the value
+        /// </summary>
+        /// <param name="x">Value to get the hash code of</param>
+        /// <returns>The hash code of x</returns>
+        [Pure]
+        public int GetHashCode(string x) =>
+            default(HashableString).GetHashCode(x);
+            
+        [Pure]
+        public Task<bool> EqualsAsync(string x, string y) =>
+            Equals(x, y).AsTask();
+
+        [Pure]
+        public Task<int> GetHashCodeAsync(string x) =>
+            GetHashCode(x).AsTask();
+    }
+    
+    /// <summary>
+    /// String equality (invariant culture, ignore case)
+    /// </summary>
+    public struct EqStringInvariantCultureIgnoreCase : Eq<string>
+    {
+        public static readonly EqStringInvariantCultureIgnoreCase Inst = default;
+
+        /// <summary>
+        /// Equality test
+        /// </summary>
+        /// <param name="a">The left hand side of the equality operation</param>
+        /// <param name="b">The right hand side of the equality operation</param>
+        /// <returns>True if a and b are equal</returns>
+        [Pure]
+        public bool Equals(string a, string b) =>
+            StringComparer.InvariantCultureIgnoreCase.Equals(a, b);
+
+        /// <summary>
+        /// Get hash code of the value
+        /// </summary>
+        /// <param name="x">Value to get the hash code of</param>
+        /// <returns>The hash code of x</returns>
+        [Pure]
+        public int GetHashCode(string x) =>
+            default(HashableString).GetHashCode(x);
+            
+        [Pure]
+        public Task<bool> EqualsAsync(string x, string y) =>
+            Equals(x, y).AsTask();
+
+        [Pure]
+        public Task<int> GetHashCodeAsync(string x) =>
+            GetHashCode(x).AsTask();
+    }
 
     /// <summary>
     /// String equality (ordinal, ignore case)
     /// </summary>
     public struct EqStringOrdinalIgnoreCase : Eq<string>
     {
-        public static readonly EqStringOrdinalIgnoreCase Inst = default(EqStringOrdinalIgnoreCase);
+        public static readonly EqStringOrdinalIgnoreCase Inst = default;
 
         /// <summary>
         /// Equality test
@@ -80,7 +150,7 @@ namespace LanguageExt.ClassInstances
     /// </summary>
     public struct EqStringOrdinal : Eq<string>
     {
-        public static readonly EqStringOrdinal Inst = default(EqStringOrdinal);
+        public static readonly EqStringOrdinal Inst = default;
 
         /// <summary>
         /// Equality test
@@ -115,7 +185,7 @@ namespace LanguageExt.ClassInstances
     /// </summary>
     public struct EqStringCurrentCultureIgnoreCase : Eq<string>
     {
-        public static readonly EqStringCurrentCultureIgnoreCase Inst = default(EqStringCurrentCultureIgnoreCase);
+        public static readonly EqStringCurrentCultureIgnoreCase Inst = default;
 
         /// <summary>
         /// Equality test
@@ -150,7 +220,7 @@ namespace LanguageExt.ClassInstances
     /// </summary>
     public struct EqStringCurrentCulture : Eq<string>
     {
-        public static readonly EqStringCurrentCulture Inst = default(EqStringCurrentCulture);
+        public static readonly EqStringCurrentCulture Inst = default;
 
         /// <summary>
         /// Equality test
