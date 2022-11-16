55fe127f2d48699916cb397d826acb138556b9e3
Paul Louth <p.louth@gmail.com>
2022-11-16 14:07:52 +0000

    Cherry pick

--- Samples/TestBed/Program.cs
+++ Samples/TestBed/Program.cs
@@ -72,7 +72,30 @@ public class Program
         //testing.Run(Runtime.New());
         //var _ = QueueExample<Runtime>.Issue1065().RunUnit(new Runtime()).Result;
         //ScheduleTests.Run();
-        ApplicativeTest.Test().GetAwaiter().GetResult();
+
+        Aff<Unit> delay(int milliseconds) =>
+            Aff(async () =>
+            {
+                await Task.Delay(milliseconds);
+                return unit;
+            });
+
+        Aff<A> delayed<A>(A x) =>
+            from _ in delay(1000)
+            select x;
+
+        var effect = SuccessAff((string sx, string sy) => 
+                                    from x in parseInt(sx)
+                                    from y in parseInt(sy)
+                                    select x + y)
+                        .Apply(delayed("100"))
+                        .Apply(delayed("200"));
+
+        var sw = Stopwatch.StartNew();
+        var r = effect.Run().GetAwaiter().GetResult();
+        sw.Stop();
+        
+        Console.Write($"Result: {r} in {sw.ElapsedMilliseconds}ms");        
     }
 
     public static async Task PipesTest()
