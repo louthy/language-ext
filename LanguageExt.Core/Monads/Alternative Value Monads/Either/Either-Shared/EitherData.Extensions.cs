6ce914804efd9887632e769ded02bd3096abc42e
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2022-12-20 16:51:19 +0100

    Add EitherDataExtensions
    Methods ToEither() and ToEitherAsync()

---
+++ LanguageExt.Core/Monads/Alternative Value Monads/Either/Either-Shared/EitherData.Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LanguageExt.DataTypes.Serialisation
+{
+    public static class EitherDataExtensions
+    {
+        public static Either<L, R> ToEither<L, R>(this EitherData<L, R> input) =>
+            input.State switch
+            {
+                EitherStatus.IsBottom => Either<L, R>.Bottom,
+                EitherStatus.IsRight => Either<L, R>.Right(input.Right),
+                EitherStatus.IsLeft => Either<L, R>.Left(input.Left),
+                _ => throw new InvalidOperationException($"Invalid {nameof(EitherStatus)} with value: {input.State}"),
+            };
+
+        public static EitherAsync<L, R> ToEitherAsync<L, R>(this EitherData<L, R> input) =>
+            input.ToEither().ToAsync();
+    }
+}
