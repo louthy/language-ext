9f6660c25387aeb501c56b96c4f5d2d0fdd85058
louthy <p.louth@gmail.com>
2023-02-04 03:55:58 +0000

    v4.4.1

--- pack.bat
+++ pack.bat
@@ -1,25 +1,25 @@
-C:
+Q:
 
 echo building the docs
 
-cd C:\dev\best-form\BestForm\bin\Release\net5.0
-bestform.exe "LanguageExt.Core" "C:\Dev\language-ext\LanguageExt.Core" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Transformers" "C:\Dev\language-ext\LanguageExt.Transformers" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Parsec" "C:\Dev\language-ext\LanguageExt.Parsec" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.FSharp" "C:\Dev\language-ext\LanguageExt.FSharp" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Rx" "C:\Dev\language-ext\LanguageExt.Rx" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.Sys" "C:\Dev\language-ext\LanguageExt.Sys" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
-bestform.exe "LanguageExt.SysX" "C:\Dev\language-ext\LanguageExt.SysX" "C:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+cd Q:\dev\best-form\BestForm\bin\Release\net5.0
+bestform.exe "LanguageExt.Core" "Q:\Dev\language-ext\LanguageExt.Core" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Transformers" "Q:\Dev\language-ext\LanguageExt.Transformers" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Parsec" "Q:\Dev\language-ext\LanguageExt.Parsec" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.FSharp" "Q:\Dev\language-ext\LanguageExt.FSharp" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Rx" "Q:\Dev\language-ext\LanguageExt.Rx" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.Sys" "Q:\Dev\language-ext\LanguageExt.Sys" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
+bestform.exe "LanguageExt.SysX" "Q:\Dev\language-ext\LanguageExt.SysX" "Q:\dev\louthy.github.io\language-ext" "https://github.com/louthy/language-ext/tree/main"
 
 echo committing them to git
 
-cd C:\dev\louthy.github.io
+cd Q:\dev\louthy.github.io
 
 git add .
 git commit -m "Language-ext documentation update"
 git push
 
-cd C:\dev\language-ext
+cd Q:\dev\language-ext
 
 echo building the artefacts
 
