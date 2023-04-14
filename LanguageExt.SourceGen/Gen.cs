566deeba200e8c1356f21ef7cf24a36b23df0c84
louthy <p.louth@gmail.com>
2023-04-14 10:10:10 +0100

    WIP: Source Generators

---
+++ LanguageExt.SourceGen/Gen.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Collections;
+using Microsoft.CodeAnalysis.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace LanguageExt.SourceGen;
+
+public class Gen : ISourceGenerator
+{
+    static readonly string[] attrNames =
+    {
+        "Union", 
+        "Record", 
+        "Free", 
+        "Reader", 
+        "RWS"
+    }; 
+    
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var types = FindTypes(context.Compilation.SyntaxTrees).ToArray();
+        
+        // TODO: Use the types to gen code
+    }
+
+    static IEnumerable<MemberDeclarationSyntax> FindTypes(IEnumerable<SyntaxTree> trees) =>
+        trees.SelectMany(FindTypes);
+
+    static IEnumerable<MemberDeclarationSyntax> FindTypes(SyntaxTree tree) =>
+        tree.GetRoot()
+            .DescendantNodes()
+            .SelectMany(FindTypes);
+
+    static IEnumerable<MemberDeclarationSyntax> FindTypes(SyntaxNode node) =>
+        node switch
+        {
+            RecordDeclarationSyntax @record       => FindTypesAndSubTypes(@record),
+            ClassDeclarationSyntax @class         => FindTypesAndSubTypes(@class),
+            InterfaceDeclarationSyntax @interface => FindTypes(@interface),
+            StructDeclarationSyntax @struct       => FindTypes(@struct),
+            NamespaceDeclarationSyntax @namespace => @namespace.Members.SelectMany(FindTypes), 
+            _                                     => Array.Empty<MemberDeclarationSyntax>()
+        };
+
+    static IEnumerable<MemberDeclarationSyntax> FindTypesAndSubTypes(MemberDeclarationSyntax node) =>
+        node.AttributeLists.Any(als => als.Attributes.Any(a => HasKnownName(a.Name)))
+            ? new[] {node}.Concat(node.DescendantNodes().SelectMany(FindTypes))
+            : node.DescendantNodes().SelectMany(FindTypes);
+
+    static IEnumerable<MemberDeclarationSyntax> FindTypes(MemberDeclarationSyntax node) =>
+        node.AttributeLists.Any(als => als.Attributes.Any(a => HasKnownName(a.Name)))
+            ? new [] { node }
+            : Array.Empty<MemberDeclarationSyntax>();
+
+    static bool HasKnownName(NameSyntax name) =>
+        name switch
+        {
+            SimpleNameSyntax n => attrNames.Contains(n.ToString()),
+            QualifiedNameSyntax qn when HasKnownName(qn.Right) => qn.Left.ToString() == "LanguageExt",
+            _ => false
+        };
+}
