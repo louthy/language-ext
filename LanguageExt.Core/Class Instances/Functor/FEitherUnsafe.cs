1ebb5805956448a4925f376292a9745e68825a30
Paul Louth <p.louth@gmail.com>
2023-08-07 14:07:52 +0100

    Fix for EitherUnsafe MapLeft throws ResultIsNullException
    https://github.com/louthy/language-ext/issues/1225

--- LanguageExt.Core/Class Instances/Functor/FEitherUnsafe.cs
+++ LanguageExt.Core/Class Instances/Functor/FEitherUnsafe.cs
@@ -8,13 +8,13 @@ namespace LanguageExt.ClassInstances
         Functor<EitherUnsafe<L, R>, EitherUnsafe<L, R2>, R, R2>,
         BiFunctor<EitherUnsafe<L, R>, EitherUnsafe<L, R2>, L, R, R2>
     {
-        public static readonly FEitherUnsafe<L, R, R2> Inst = default(FEitherUnsafe<L, R, R2>);
+        public static readonly FEitherUnsafe<L, R, R2> Inst = default;
 
         [Pure]
         public EitherUnsafe<L, R2> BiMap(EitherUnsafe<L, R> ma, Func<L, R2> fa, Func<R, R2> fb) =>
             default(MEitherUnsafe<L, R>).MatchUnsafe(ma,
-                Left: a => EitherUnsafe<L, R2>.Right(Check.NullReturn(fa(a))),
-                Right: b => EitherUnsafe<L, R2>.Right(Check.NullReturn(fb(b))),
+                Left: a => EitherUnsafe<L, R2>.Right(fa(a)),
+                Right: b => EitherUnsafe<L, R2>.Right(fb(b)),
                 Bottom: () => EitherUnsafe<L, R2>.Bottom);
 
         [Pure]
@@ -28,13 +28,13 @@ namespace LanguageExt.ClassInstances
     public struct FEitherUnsafeBi<L, R, L2, R2> :
         BiFunctor<EitherUnsafe<L, R>, EitherUnsafe<L2, R2>, L, R, L2, R2>
     {
-        public static readonly FEitherUnsafeBi<L, R, L2, R2> Inst = default(FEitherUnsafeBi<L, R, L2, R2>);
+        public static readonly FEitherUnsafeBi<L, R, L2, R2> Inst = default;
 
         [Pure]
         public EitherUnsafe<L2, R2> BiMap(EitherUnsafe<L, R> ma, Func<L, L2> fa, Func<R, R2> fb) =>
             default(MEitherUnsafe<L, R>).MatchUnsafe(ma,
-                Left: a => EitherUnsafe<L2, R2>.Left(Check.NullReturn(fa(a))),
-                Right: b => EitherUnsafe<L2, R2>.Right(Check.NullReturn(fb(b))),
+                Left: a => EitherUnsafe<L2, R2>.Left(fa(a)),
+                Right: b => EitherUnsafe<L2, R2>.Right(fb(b)),
                 Bottom: () => EitherUnsafe<L2, R2>.Bottom);
     }
 }
