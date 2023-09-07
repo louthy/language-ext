a694e8378b09d62caabe3e3db3655cb65423859e
Stefan Bertels <stefan@bertels.org>
2023-09-07 15:03:05 +0200

    adds Fin test class

---
+++ LanguageExt.Tests/FinTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.Tests
+{
+    
+    public class FinTests
+    {
+        [Fact] public void Equality()
+        {
+            var fin1 = FinSucc(1);
+            Assert.Equal(1, fin1);
+        }
+        
+        [Fact] public void Enumerability()
+        {
+            var fin1 = FinSucc(1);
+            Assert.Equal(1, fin1.HeadOrNone());
+        }
+    }
+}
