702aefc230c1d4efcaae65a5ead97e9bdec5634b
Paul Louth <p.louth@gmail.com>
2024-01-29 19:36:07 +0000

    Extensions nullable fixup

--- LanguageExt.Core/Extensions/ActionObservable.cs
+++ LanguageExt.Core/Extensions/ActionObservable.cs
@@ -1,35 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LanguageExt
+namespace LanguageExt;
+
+/// <summary>
+/// Executes an action post-subscription.  This is useful when the action is 
+/// going to publish to the observable.  A kind of request/response.
+/// Use the IObservable extension method: PostSubscribe(() => ...)
+/// </summary>
+public class ActionObservable<T>(
+    Action PostSubscribeAction,
+    IObservable<T> SwitchTo)
+    : IObservable<T>
 {
-    /// <summary>
-    /// Executes an action post-subscription.  This is useful when the action is 
-    /// going to publish to the observable.  A kind of request/response.
-    /// Use the IObservable extension method: PostSubscribe(() => ...)
-    /// </summary>
-    public class ActionObservable<T> : IObservable<T>
+    public IDisposable Subscribe(IObserver<T> observer)
     {
-        readonly Action postSubscribeAction;
-        readonly IObservable<T> switchTo;
-
-        public ActionObservable(
-            Action postSubscribeAction,
-            IObservable<T> switchTo
-            )
-        {
-            this.postSubscribeAction = postSubscribeAction;
-            this.switchTo = switchTo;
-        }
-
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            var subs = switchTo.Subscribe(observer);
-            postSubscribeAction();
-            return subs;
-        }
+        var subs = SwitchTo.Subscribe(observer);
+        PostSubscribeAction();
+        return subs;
     }
 }
--- LanguageExt.Core/Extensions/ObservableExt.cs
+++ LanguageExt.Core/Extensions/ObservableExt.cs
@@ -6,103 +6,96 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using LanguageExt.Common;
 
-namespace LanguageExt
+namespace LanguageExt;
+
+/// <summary>
+/// Observable extensions
+/// </summary>
+public static class ObservableExt
 {
     /// <summary>
-    /// Observable extensions
+    /// Executes an action post-subscription.  This is useful when the action is 
+    /// going to publish to the observable.  A kind of request/response.
     /// </summary>
-    public static class ObservableExt
+    [Pure]
+    public static IObservable<T> PostSubscribe<T>(
+        this IObservable<T> self,
+        Action action) =>
+        new ActionObservable<T>(action, self);
+
+    /// <summary>
+    /// Executes an action post-subscription.  This is useful when the action is 
+    /// going to publish to the observable.  A kind of request/response.
+    /// </summary>
+    [Pure]
+    public static IObservable<T> PostSubscribe<T>(
+        this IObservable<T> self,
+        Func<Unit> action) =>
+        new ActionObservable<T>(() => action(), self);
+
+    /// <summary>
+    /// Convert an `IObservable` to an `IAsyncEnumerable`
+    /// </summary>
+    public static IAsyncEnumerable<A> ToAsyncEnumerable<A>(
+        this IObservable<A> observable,
+        CancellationToken token) =>
+        Observe<A>.Run(observable, token);
+
+    class Observe<A> : IObserver<A>
     {
-        /// <summary>
-        /// Executes an action post-subscription.  This is useful when the action is 
-        /// going to publish to the observable.  A kind of request/response.
-        /// </summary>
-        [Pure]
-        public static IObservable<T> PostSubscribe<T>(
-            this IObservable<T> self,
-            Action action
-            )
+        readonly AutoResetEvent wait;
+        readonly ConcurrentQueue<Fin<A>> queue;
+
+        Observe(AutoResetEvent wait, ConcurrentQueue<Fin<A>> queue)
         {
-            return new ActionObservable<T>(action, self);
+            this.wait = wait;
+            this.queue = queue;
         }
 
-        /// <summary>
-        /// Executes an action post-subscription.  This is useful when the action is 
-        /// going to publish to the observable.  A kind of request/response.
-        /// </summary>
-        [Pure]
-        public static IObservable<T> PostSubscribe<T>(
-            this IObservable<T> self,
-            Func<Unit> action
-            )
+        public static async IAsyncEnumerable<A> Run(
+            IObservable<A> observable, 
+            [EnumeratorCancellation] CancellationToken token)
         {
-            return new ActionObservable<T>(() => action(), self);
-        }
+            using var wait  = new AutoResetEvent(false);
+            var       queue = new ConcurrentQueue<Fin<A>>();
+            observable.Subscribe(new Observe<A>(wait, queue));
 
-        /// <summary>
-        /// Convert an `IObservable` to an `IAsyncEnumerable`
-        /// </summary>
-        public static IAsyncEnumerable<A> ToAsyncEnumerable<A>(
-            this IObservable<A> observable,
-            CancellationToken token) =>
-            Observe<A>.Run(observable, token);
-
-        class Observe<A> : IObserver<A>
-        {
-            readonly AutoResetEvent wait;
-            readonly ConcurrentQueue<Fin<A>> queue;
-
-            Observe(AutoResetEvent wait, ConcurrentQueue<Fin<A>> queue)
+            while (true)
             {
-                this.wait = wait;
-                this.queue = queue;
-            }
-
-            public static async IAsyncEnumerable<A> Run(
-                IObservable<A> observable, 
-                [EnumeratorCancellation] CancellationToken token)
-            {
-                using var wait  = new AutoResetEvent(false);
-                var       queue = new ConcurrentQueue<Fin<A>>();
-                observable.Subscribe(new Observe<A>(wait, queue));
-
-                while (true)
+                await wait.WaitOneAsync(token).ConfigureAwait(false);
+                while (queue.TryDequeue(out var item))
                 {
-                    await wait.WaitOneAsync(token).ConfigureAwait(false);
-                    while (queue.TryDequeue(out var item))
+                    if (item.IsFail)
                     {
-                        if (item.IsFail)
-                        {
-                            if (item.Error == Errors.None) yield break;
-                            if (item.Error == Errors.Cancelled) throw new OperationCanceledException();
-                            item.Error.Throw();
-                            yield break;
-                        }
-                        else
-                        {
-                            yield return item.Value;
-                        }
+                        if (item.Error == Errors.None) yield break;
+                        if (item.Error == Errors.Cancelled) throw new OperationCanceledException();
+                        item.Error.Throw();
+                        yield break;
                     }
-                }                
-            }
+                    else
+                    {
+                        yield return item.Value;
+                    }
+                }
+            }                
+        }
 
-            public void OnCompleted()
-            {
-                queue.Enqueue(Errors.None);
-                wait.Set();
-            }
+        public void OnCompleted()
+        {
+            queue.Enqueue(Errors.None);
+            wait.Set();
+        }
 
-            public void OnError(Exception error)
-            {
-                queue.Enqueue(Error.New(error));
-                wait.Set();
-            }
+        public void OnError(Exception error)
+        {
+            queue.Enqueue(Error.New(error));
+            wait.Set();
+        }
 
-            public void OnNext(A value)
-            {
-                queue.Enqueue(value);
-                wait.Set();
-            }
+        public void OnNext(A value)
+        {
+            queue.Enqueue(value);
+            wait.Set();
         }
     }
 }
