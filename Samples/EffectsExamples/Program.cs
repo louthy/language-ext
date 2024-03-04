8816853bc665894961abedd9d083b4722d1e8335
Paul Louth <p.louth@gmail.com>
2024-03-04 20:53:58 +0000

    Restore EffectsExample main

--- Samples/EffectsExamples/Program.cs
+++ Samples/EffectsExamples/Program.cs
@@ -7,26 +7,9 @@ namespace EffectsExamples;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Test();
-        return;
-        
+    static void Main(string[] args) =>
         Menu<Runtime>
            .menu
            .Run(Runtime.New(), EnvIO.New())
            .ThrowIfFail();
-    }
-
-    static void Test()
-    {
-        var mx = OptionT.lift(IO.lift<int>(() => throw new Exception("Fail")));
-        
-        //var my = mx.MapFail(e => Error.New("Replacement error"));
-        //var my = mx.IfNone(100).As();
-        var my = mx.MapT(io => io.As().MapFail(e => Error.New("Replacement error")));
-        
-        var r  = my.Run().As().Run();
-        Console.WriteLine(r);
-    }
 }
