f0635e2895424e07657454eb63021cda45281bc3
Paul Louth <p.louth@gmail.com>
2024-03-05 17:36:27 +0000

    Parallel applicatives

--- LanguageExt.Core/Effects/Eff/Eff with runtime/Extensions/Eff.Extensions.Apply.cs
+++ LanguageExt.Core/Effects/Eff/Eff with runtime/Extensions/Eff.Extensions.Apply.cs
@@ -13,7 +13,11 @@ public static partial class EffExtensions
     public static Eff<RT, B> Apply<RT, A, B>(
         this K<Eff<RT>, Func<A, B>> mf,
         K<Eff<RT>, A> ma) =>
-        mf.Bind(ma.Map).As();
+        from tf in mf.As().Fork()
+        from ta in ma.As().Fork()
+        from f in tf.Await
+        from a in ta.Await
+        select f(a);
     
     /// <summary>
     /// Applicative apply: takes the lifted function and the lifted argument, applies the function to the argument
