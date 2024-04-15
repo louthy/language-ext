11aa33e213b8abe952e5e8874fddb27e135af204
Paul Louth <p.louth@gmail.com>
2024-04-15 21:28:58 +0100

    Applicative lift update

--- LanguageExt.Core/Traits/Applicative/Applicative.Module.cs
+++ LanguageExt.Core/Traits/Applicative/Applicative.Module.cs
@@ -72,20 +72,20 @@ public static class Applicative
     [Pure]
     public static K<F, C> lift<F, A, B, C>(Func<A, B, C> f, K<F, A> fa, K<F, B> fb)
         where F : Applicative<F> =>
-        apply(F.Pure(f), fa, fb);
+        f.Map(fa).Apply(fb);
 
     [Pure]
     public static K<F, C> lift<F, A, B, C>(Func<A, Func<B, C>> f, K<F, A> fa, K<F, B> fb)
         where F : Applicative<F> =>
-        apply(apply(F.Pure(f), fa), fb);
+        f.Map(fa).Apply(fb);
 
     [Pure]
     public static K<F, D> lift<F, A, B, C, D>(Func<A, B, C, D> f, K<F, A> fa, K<F, B> fb, K<F, C> fc)
         where F : Applicative<F> =>
-        apply(F.Pure(f), fa, fb, fc);
+        f.Map(fa).Apply(fb).Apply(fc);
 
     [Pure]
     public static K<F, D> lift<F, A, B, C, D>(Func<A, Func<B, Func<C, D>>> f, K<F, A> fa, K<F, B> fb, K<F, C> fc)
         where F : Applicative<F> =>
-        apply(apply(apply(F.Pure(f), fa), fb), fc);
+        f.Map(fa).Apply(fb).Apply(fc);
 }
