e4543790ee1a2fefa5fa6bbf25763a07b6a21f5d
Paul Louth <p.louth@gmail.com>
2024-08-16 17:43:38 +0100

    Updated TestBed.Web.Runner
    Tests both sync and async versions

--- Samples/TestBed.Web/Program.cs
+++ Samples/TestBed.Web/Program.cs
@@ -3,15 +3,32 @@ using static LanguageExt.Prelude;
 var builder = WebApplication.CreateBuilder(args);
 var app     = builder.Build();
 
-app.MapGet("/", async () =>
-                {
-                    var effect = liftIO(async () =>
-                                        {
-                                            await Task.Delay(1000).ConfigureAwait(false);
-                                            return unit;
-                                        });
+app.MapGet("/sync", () =>
+                    {
+                        var effect = liftIO(async () =>
+                                            {
+                                                await Task.Delay(1000).ConfigureAwait(false);
+                                                return unit;
+                                            });
 
-                    await effect.RunAsync();
-                });
+                        // TODO: This is 5x slower than the RunAsync version below
+                        // TODO: Make it faster
+                        var computation = from f in effect.Fork()
+                                          from r in f.Await
+                                          select r;
+
+                        return computation.Run();
+                    });
+
+app.MapGet("/async", async () =>
+                     {
+                         var effect = liftIO(async () =>
+                                             {
+                                                 await Task.Delay(1000).ConfigureAwait(false);
+                                                 return unit;
+                                             });
+
+                         return await effect.RunAsync();
+                     });
 
 app.Run();
