8eac5e7379766990aeb58ef695223f39d95d4560
Paul Louth <p.louth@gmail.com>
2024-08-16 21:37:48 +0100

    Elegance

--- Samples/Streams/Folding.cs
+++ Samples/Streams/Folding.cs
@@ -7,14 +7,14 @@ namespace Streams;
 public static class Folding
 {
     public static IO<Unit> run =>
-        from x in example(1000).Iter().As()
+        from x in example(100).Iter().As()
         select unit;
 
-    static StreamT<IO, int> naturals =>
-        Range(0, int.MaxValue).AsStream<IO>();
+    static StreamT<IO, int> naturals(int n) =>
+        Range(0, n).AsStream<IO>();
     
     static StreamT<IO, Unit> example(int n) =>
-        from v in naturals.FoldUntil(0, (s, x) => s + x, (s, _) => s % 10 == 0)
+        from v in naturals(n).FoldUntil(0, (s, x) => s + x, (_, x) => x % 10 == 0)
         from _ in writeLine(v.ToString())
         where false
         select unit;
