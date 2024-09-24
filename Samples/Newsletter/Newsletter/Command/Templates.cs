eec4c407e7726162b267fe4237ba5edd4728e302
Paul Louth <p.louth@gmail.com>
2024-09-24 12:13:06 +0100

    Tidying of sample

---
+++ Samples/Newsletter/Newsletter/Command/Templates.cs
@@ -0,0 +1,30 @@
+using Newsletter.Data;
+using Newsletter.Effects;
+
+namespace Newsletter.Command;
+
+/// <summary>
+/// Loads the templates for the emails
+/// </summary>
+public static class Templates<M, RT>
+    where RT : 
+        Has<M, FileIO>,
+        Has<M, EncodingIO>,
+        Has<M, DirectoryIO>,
+        Has<M, Config>
+    where M :
+        Monad<M>,
+        Fallible<M>
+{
+    public static K<M, Templates> loadDefault =>
+        from folder in Config<M, RT>.templateFolder
+        from email in loadTemplate("email")
+        from recnt in loadTemplate("recent-item")
+        select new Templates(email, recnt);
+    
+    public static K<M, Template> loadTemplate(string name) =>
+        from fldr in Config<M, RT>.templateFolder
+        from html in File<M, RT>.readAllText(Path.Combine(fldr, $"{name}.html"))
+        from text in File<M, RT>.readAllText(Path.Combine(fldr, $"{name}.txt"))
+        select new Template(html, text);
+}
--- Samples/Newsletter/Newsletter/Data/Members.cs
+++ Samples/Newsletter/Newsletter/Data/Members.cs
@@ -1,66 +1,6 @@
-using CsvHelper;
-using CsvHelper.Configuration;
-using System.Globalization;
-using Newsletter.Effects;
-
 namespace Newsletter.Data;
 
 /// <summary>
 /// Member record
 /// </summary>
 public record Member(string Id, string Email, string Name, bool SubscribedToEmails, bool Supporter);
-
-/// <summary>
-/// Member access
-/// </summary>
-public static class Members<M, RT>
-    where RT : 
-        Has<M, FileIO>,
-        Has<M, EncodingIO>,
-        Has<M, DirectoryIO>,
-        Has<M, Config>
-    where M :
-        Monad<M>,
-        Fallible<M>
-{
-    public static K<M, Seq<Member>> readAll =>
-        from folder  in Config<M, RT>.membersFolder
-        from path    in readFirstFile(folder)
-        select readMembers(path).Filter(m => m.SubscribedToEmails);
-
-    static K<M, string> readFirstFile(string folder) =>
-        Directory<M, RT>.enumerateFiles(folder, "*.csv")
-                        .Map(fs => fs.OrderDescending()
-                                     .AsIterable()
-                                     .Head())
-                        .Bind(path => path.Match(
-                                  Some: pure<M, string>,
-                                  None: error<M, string>(Error.New($"no member files found in {folder}"))));
-
-    static Seq<Member> readMembers(string path)
-    {
-        var       config  = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = true };
-        using var reader  = new StreamReader(path);
-        using var csv     = new CsvReader(reader, config);
-        return csv.GetRecords<Row>()
-                  .AsIterable()
-                  .Map(r => new Member(r.id, r.email, r.name, r.subscribed_to_emails == "true", r.tiers == "Supporter"))
-                  .ToSeq()
-                  .Strict();
-    }
-
-    record Row(
-        string id,
-        string email,
-        string name,
-        string note,
-        string subscribed_to_emails,
-        string complimentary_plan,
-        string stripe_customer_id,
-        string created_at,
-        string deleted_at,
-        string labels,
-        string tiers);
-}
-
-
