bf0f5e2bc1a06a8b012035c12a12f14fe140e1ef
Paul Louth <p.louth@gmail.com>
2024-10-15 22:10:41 +0100

    Monad laws

---
+++ LanguageExt.Core/Traits/Monads/Monad/Monad.Laws.cs
@@ -0,0 +1,116 @@
+using System;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+namespace LanguageExt.Traits;
+
+/// <summary>
+/// Functions that test that monad laws hold for the `F` monad provided.
+/// </summary>
+/// <para>
+///  * Homomorphism: pure g <*> pure x ≡ pure (g x)
+///  * Identity: pure id <*> xs ≡ xs
+///  * Interchange: fs <*> pure x ≡ pure ($ x) <*> fs
+///  * monad: g <$> xs ≡ pure g <*> xs
+///  * Composition: (.) <$> us <*> vs <*> xs ≡ us <*> (vs <*> xs)
+/// </para>
+/// <remarks>
+/// NOTE: `Equals` must be implemented for the `K<F, *>` derived-type, so that the laws
+/// can be proven to be true.  If your monad doesn't have `Equals` then you must provide
+/// the optional `equals` parameter so that the equality of outcomes can be tested.
+/// </remarks>
+/// <typeparam name="F">Functor type</typeparam>
+public static class MonadLaw<F>
+    where F : Monad<F>
+{
+    /// <summary>
+    /// Assert that the monad laws hold
+    /// </summary>
+    /// <remarks>
+    /// NOTE: `Equals` must be implemented for the `K<F, *>` derived-type, so that the laws
+    /// can be proven to be true.  If your monad doesn't have `Equals` then you must provide
+    /// the optional `equals` parameter so that the equality of outcomes can be tested.
+    /// </remarks>
+    public static Unit assert(Func<K<F, int>, K<F, int>, bool>? equals = null) =>
+        validate(equals)
+           .IfFail(errors => errors.Throw());
+
+    /// <summary>
+    /// Validate that the monad laws hold
+    /// </summary>
+    /// <remarks>
+    /// NOTE: `Equals` must be implemented for the `K<F, *>` derived-type, so that the laws
+    /// can be proven to be true.  If your monad doesn't have `Equals` then you must provide
+    /// the optional `equals` parameter so that the equality of outcomes can be tested.
+    /// </remarks>
+    public static Validation<Error, Unit> validate(Func<K<F, int>, K<F, int>, bool>? equals = null)
+    {
+        equals ??= (fa, fb) => fa.Equals(fb);
+        return ApplicativeLaw<F>.validate(equals) >>
+               leftIdentityLaw(equals)            >>
+               rightIdentityLaw(equals)           >>
+               associativityLaw(equals);
+    }
+
+    /// <summary>
+    /// Validate the left-identity law
+    /// </summary>
+    /// <remarks>
+    /// NOTE: `Equals` must be implemented for the `K<F, *>` derived-type, so that the laws
+    /// can be proven to be true.  If your monad doesn't have `Equals` then you must provide
+    /// the optional `equals` parameter so that the equality of outcomes can be tested.
+    /// </remarks>
+    public static Validation<Error, Unit> leftIdentityLaw(Func<K<F, int>, K<F, int>, bool> equals)
+    {
+        var a   = 100;
+        var h   = (int x) => F.Pure(x);
+        var lhs = F.Pure(a).Bind(h);
+        var rhs = h(a);
+        
+        return equals(lhs, rhs) 
+                   ? unit
+                   : Error.New($"Monad left-identity law does not hold for {typeof(F).Name}");
+    }
+
+    /// <summary>
+    /// Validate the right-identity law
+    /// </summary>
+    /// <remarks>
+    /// NOTE: `Equals` must be implemented for the `K<F, *>` derived-type, so that the laws
+    /// can be proven to be true.  If your monad doesn't have `Equals` then you must provide
+    /// the optional `equals` parameter so that the equality of outcomes can be tested.
+    /// </remarks>
+    public static Validation<Error, Unit> rightIdentityLaw(Func<K<F, int>, K<F, int>, bool> equals)
+    {
+        var a   = 100;
+        var m   = F.Pure(a);
+        var lhs = m.Bind(F.Pure);
+        var rhs = m;
+        
+        return equals(lhs, rhs) 
+                   ? unit
+                   : Error.New($"Monad right-identity law does not hold for {typeof(F).Name}");
+    }
+
+    /// <summary>
+    /// Validate the associativity law
+    /// </summary>
+    /// <remarks>
+    /// NOTE: `Equals` must be implemented for the `K<F, *>` derived-type, so that the laws
+    /// can be proven to be true.  If your monad doesn't have `Equals` then you must provide
+    /// the optional `equals` parameter so that the equality of outcomes can be tested.
+    /// </remarks>
+    public static Validation<Error, Unit> associativityLaw(Func<K<F, int>, K<F, int>, bool> equals)
+    {
+        var m = F.Pure(100);
+        var g = (int x) => F.Pure(x * 10);
+        var h = (int x) => F.Pure(x + 83);
+        
+        var lhs = m.Bind(g).Bind(h);
+        var rhs = m.Bind(x => g(x).Bind(h));
+        
+        return equals(lhs, rhs) 
+                   ? unit
+                   : Error.New($"Monad associativity law does not hold for {typeof(F).Name}");
+    }
+
+}
---
+++ LanguageExt.Tests/TraitTests/MonadLawsTest.cs
@@ -0,0 +1,159 @@
+using Xunit;
+
+namespace LanguageExt.Tests.TraitTests;
+
+public class MonadLawsTest
+{
+    [Fact]
+    public void EffRT()
+    {
+        bool eq(K<Eff<Unit>, int> vx, K<Eff<Unit>, int> vy) => 
+            vx.Run(unit).Equals(vy.Run(unit));
+        
+        MonadLaw<Eff<Unit>>.assert(eq);
+    }
+    
+    [Fact]
+    public void Eff()
+    {
+        bool eq(K<Eff, int> vx, K<Eff, int> vy) => 
+            vx.Run().Equals(vy.Run());
+        
+        MonadLaw<Eff>.assert(eq);
+    }
+    
+    [Fact]
+    public void IO()
+    {
+        bool eq(K<IO, int> vx, K<IO, int> vy) => 
+            vx.RunSafe().Equals(vy.RunSafe());
+        
+        MonadLaw<IO>.assert(eq);
+    }
+    
+    [Fact]
+    public void StreamT()
+    {
+        static Seq<int> toSeq(StreamT<Identity, int> s) =>
+            s.Head().Run()
+             .Match(Some: x => x.Cons(toSeq(s.Tail())),
+                    None: []);
+
+        static bool eq(K<StreamT<Identity>, int> vx, K<StreamT<Identity>, int> vy) =>
+            toSeq(vx.As()) == toSeq(vy.As());
+        
+        MonadLaw<StreamT<Identity>>.assert(eq);
+    }
+    
+    [Fact]
+    public void Either() => 
+        MonadLaw<Either<string>>.assert();
+
+    [Fact]
+    public void EitherT() => 
+        MonadLaw<EitherT<string, Identity>>.assert();
+
+    [Fact]
+    public void Fin() => 
+        MonadLaw<Fin>.assert();
+
+    [Fact]
+    public void FinT() => 
+        MonadLaw<FinT<Identity>>.assert();
+
+    [Fact]
+    public void Option() => 
+        MonadLaw<Option>.assert();
+
+    [Fact]
+    public void OptionT() => 
+        MonadLaw<OptionT<Identity>>.assert();
+
+    [Fact]
+    public void Try()
+    {
+        bool eq(K<Try, int> vx, K<Try, int> vy) => 
+            vx.Run().Equals(vy.Run());
+        
+        MonadLaw<Try>.assert(eq);
+    }
+    
+    [Fact]
+    public void TryT()
+    {
+        bool eq(K<TryT<Identity>, int> vx, K<TryT<Identity>, int> vy) => 
+            vx.Run().Run().Equals(vy.Run().Run());
+        
+        MonadLaw<TryT<Identity>>.assert(eq);
+    }
+    
+    [Fact]
+    public void Validation() => 
+        MonadLaw<Validation<StringM>>.assert();
+
+    [Fact]
+    public void ValidationT() => 
+        MonadLaw<ValidationT<StringM, Identity>>.assert();
+
+    [Fact]
+    public void Identity() => 
+        MonadLaw<Identity>.assert();
+
+    [Fact]
+    public void IdentityT() => 
+        MonadLaw<IdentityT<Identity>>.assert();
+
+    [Fact]
+    public void Reader()
+    {
+        bool eq(K<Reader<string>, int> vx, K<Reader<string>, int> vy) => 
+            vx.Run("Hello").Equals(vy.Run("Hello"));
+        
+        MonadLaw<Reader<string>>.assert(eq);
+    }
+    
+    [Fact]
+    public void ReaderT()
+    {
+        bool eq(K<ReaderT<string, Identity>, int> vx, K<ReaderT<string, Identity>, int> vy) => 
+            vx.Run("Hello").Run().Equals(vy.Run("Hello").Run());
+                 
+        MonadLaw<ReaderT<string, Identity>>.assert(eq);
+    }
+
+    [Fact]
+    public void State()
+    {
+        bool eq(K<State<string>, int> vx, K<State<string>, int> vy) => 
+            vx.Run("Hello").Equals(vy.Run("Hello"));
+        
+        MonadLaw<State<string>>.assert(eq);
+    }
+    
+    [Fact]
+    public void StateT()
+    {
+        bool eq(K<StateT<string, Identity>, int> vx, K<StateT<string, Identity>, int> vy) => 
+            vx.Run("Hello").Run().Equals(vy.Run("Hello").Run());
+                 
+        MonadLaw<StateT<string, Identity>>.assert(eq);
+    }
+    
+    [Fact]
+    public void Writer()
+    {
+        bool eq(K<Writer<StringM>, int> vx, K<Writer<StringM>, int> vy) => 
+            vx.Run("Hello, ").Equals(vy.Run("Hello, "));
+        
+        MonadLaw<Writer<StringM>>.assert(eq);
+    }
+    
+    [Fact]
+    public void WriterT()
+    {
+        bool eq(K<WriterT<StringM, Identity>, int> vx, K<WriterT<StringM, Identity>, int> vy) => 
+            vx.Run("Hello, ").Run().Equals(vy.Run("Hello, ").Run());
+        
+        MonadLaw<WriterT<StringM, Identity>>.assert(eq);
+    }    
+}
