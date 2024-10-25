ebcf7717d59433014d6095ca9e4a57b85db7034f
Paul Louth <p.louth@gmail.com>
2024-10-25 10:39:21 +0100

    Allow an EnvIO to be created from a Token

--- LanguageExt.Core/Effects/IO/EnvIO.cs
+++ LanguageExt.Core/Effects/IO/EnvIO.cs
@@ -12,19 +12,59 @@ public class EnvIO : IDisposable
     public readonly CancellationToken Token;
     public readonly CancellationTokenSource Source;
     public readonly SynchronizationContext? SyncContext;
+    private readonly CancellationTokenRegistration? Registration;
     readonly int Own;
+    int disposed;
 
     EnvIO(Resources resources,
           CancellationToken token,
           CancellationTokenSource source,
           SynchronizationContext? syncContext,
+          CancellationTokenRegistration? registration,
           int own)
     {
-        Resources   = resources;
-        Token       = token;
-        Source      = source;
-        SyncContext = syncContext;
-        Own         = own;
+        Resources    = resources;
+        Token        = token;
+        Source       = source;
+        SyncContext  = syncContext;
+        Registration = registration;
+        Own          = own;
+    }
+
+    /// <summary>
+    /// Creates a new EnvIO and registers it with the `token` provided, so if the `token` gets
+    /// a cancellation signal then so will any computation that uses the `EnvIO`. 
+    /// </summary>
+    /// <param name="token">Token to register with</param>
+    /// <returns>Constructed EnvIO</returns>
+    public static EnvIO FromToken(CancellationToken token)
+    {
+        if (!token.CanBeCanceled) return New();
+        var       nsource    = new CancellationTokenSource();
+        var       ntoken     = nsource.Token;
+        var       ncontext   = SynchronizationContext.Current;
+        using var nreg       = token.Register(() => nsource.Cancel());
+        var       nresources = new Resources(null);
+        var       own        = 3;
+        return new EnvIO(nresources, ntoken, nsource, ncontext, nreg, own);
+    }
+
+    /// <summary>
+    /// Creates a new EnvIO and registers it with the `token` provided, so if the `token` gets
+    /// a cancellation signal then so will any computation that uses the `EnvIO`. 
+    /// </summary>
+    /// <param name="resources">Resources collection to use instead of letting EnvIO construct one</param>
+    /// <param name="token">Token to register with</param>
+    /// <returns>Constructed EnvIO</returns>
+    public static EnvIO FromToken(Resources resources, CancellationToken token)
+    {
+        if (!token.CanBeCanceled) return New(resources);
+        var       nsource    = new CancellationTokenSource();
+        var       ntoken     = nsource.Token;
+        var       ncontext   = SynchronizationContext.Current;
+        using var nreg       = token.Register(() => nsource.Cancel());
+        var       own        = 1;
+        return new EnvIO(resources, ntoken, nsource, ncontext, nreg, own);
     }
 
     public static EnvIO New(
@@ -48,7 +88,7 @@ public class EnvIO : IDisposable
 
         token       =   token.CanBeCanceled ? token : source.Token;
         syncContext ??= SynchronizationContext.Current;
-        return new EnvIO(resources, token, source, syncContext, own);
+        return new EnvIO(resources, token, source, syncContext, null, own);
     }
 
     public EnvIO LocalResources =>
@@ -62,8 +102,12 @@ public class EnvIO : IDisposable
 
     public void Dispose()
     {
-        if ((Own & 2) == 2) Resources.DisposeU(this);
-        if ((Own & 1) == 1) Source.Dispose();
+        if (Interlocked.CompareExchange(ref disposed, 1, 0) == 0)
+        {
+            if ((Own & 2) == 2) Resources.DisposeU(this);
+            if ((Own & 1) == 1) Source.Dispose();
+            Registration?.Dispose();
+        }
     }
 
     public override string ToString() => 
