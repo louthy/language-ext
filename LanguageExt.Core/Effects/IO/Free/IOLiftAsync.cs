70f4ea93050214ce24ffce1b81f12c588fea2d88
Paul Louth <p.louth@gmail.com>
2024-12-19 15:20:20 +0000

    Missing from last commit

---
+++ LanguageExt.Core/Effects/IO/Free/IOLiftAsync.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using LanguageExt.Traits;
+
+namespace LanguageExt;
+    
+record IOLiftAsync<A, B>(Func<EnvIO, Task<A>> F, Func<A, K<IO, B>> Next) : InvokeAsyncIO<B>
+{
+    public override IO<C> Map<C>(Func<B, C> f) => 
+        new IOLiftAsync<A, C>(F, x => Next(x).Map(f));
+
+    public override IO<C> Bind<C>(Func<B, K<IO, C>> f) => 
+        new IOLiftAsync<A, C>(F, x => Next(x).Bind(f));
+
+    public override async ValueTask<IO<B>> Invoke(EnvIO envIO) =>
+        Next(await F(envIO)).As();
+}
---
+++ LanguageExt.Core/Effects/IO/Free/IOLiftSync.cs
@@ -0,0 +1,16 @@
+using System;
+using LanguageExt.Traits;
+
+namespace LanguageExt;
+
+record IOLiftSync<A, B>(Func<EnvIO, A> F, Func<A, K<IO, B>> Next) : InvokeSyncIO<B>
+{
+    public override IO<C> Map<C>(Func<B, C> f) => 
+        new IOLiftSync<A, C>(F, x => Next(x).Map(f));
+
+    public override IO<C> Bind<C>(Func<B, K<IO, C>> f) => 
+        new IOLiftSync<A, C>(F, x => Next(x).Bind(f));
+
+    public override IO<B> Invoke(EnvIO envIO) =>
+        Next(F(envIO)).As();
+}
