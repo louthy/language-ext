cb9d38895582a9d3bd5d2b177278f4f53f8cc9ad
Paul Louth <p.louth@gmail.com>
2025-02-10 12:25:56 +0000

    Clarify natural transformation comments and add detailed remarks
    Improved the documentation for natural transformations by refining existing comments and adding explanatory remarks. Provided additional context on functors and their connection to natural transformations for better understanding.

--- LanguageExt.Core/Traits/Natural/Natural.Module.cs
+++ LanguageExt.Core/Traits/Natural/Natural.Module.cs
@@ -7,7 +7,12 @@ public static class Natural
     /// </summary>
     /// <remarks>
     /// If functor `map` operations transform the bound-values within the structure, then
-    /// natural-transformations transform the structure itself.  
+    /// natural-transformations transform the structure itself.
+    /// </remarks>
+    /// <remarks>
+    /// Functors are referenced, because that's the true definition in category-theory, but
+    /// there is no requirement in language-ext for FA or GA to be functors.  It is just typically
+    /// true that FA and GA will also be functors.
     /// </remarks>
     /// <param name="fa">Functor to transform</param>
     /// <typeparam name="F">Source functor type</typeparam>
--- LanguageExt.Core/Traits/Natural/Natural.cs
+++ LanguageExt.Core/Traits/Natural/Natural.cs
@@ -13,8 +13,17 @@ public interface Natural<F>
     public interface Transform<in G>
     {
         /// <summary>
-        /// Perform a natural transformation from `F A -> G A`
+        /// Perform a natural transformation from `FA -> GA`
         /// </summary>
+        /// <remarks>
+        /// If functor `map` operations transform the bound-values within the structure, then
+        /// natural-transformations transform the structure itself.
+        /// </remarks>
+        /// <remarks>
+        /// Functors are referenced, because that's the true definition in category-theory, but
+        /// there is no requirement in language-ext for FA or GA to be functors.  It is just typically
+        /// true that FA and GA will also be functors.
+        /// </remarks>
         /// <param name="fa">Functor to transform</param>
         /// <typeparam name="A">Bound value type</typeparam>
         /// <returns>Transformed functor</returns>
