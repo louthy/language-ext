3f599779a44139df431b0d5b9d02f9f46f924a38
Paul Louth <p.louth@gmail.com>
2025-02-10 13:04:07 +0000

    Add detailed remarks to explain natural transformations
    Expanded the documentation for the `Natural` interface by adding remarks. These remarks clarify the concept of natural transformations, their relation to functors, and their role in category theory, improving the overall comprehensibility of the code.

--- LanguageExt.Core/Traits/Natural/Natural.cs
+++ LanguageExt.Core/Traits/Natural/Natural.cs
@@ -3,6 +3,15 @@ namespace LanguageExt.Traits;
 /// <summary>
 /// Natural transformation 
 /// </summary>
+/// <remarks>
+/// If functor `map` operations transform the bound-values within the structure, then
+/// natural-transformations transform the structure itself.
+/// </remarks>
+/// <remarks>
+/// Functors are referenced, because that's the true definition in category-theory, but
+/// there is no requirement in language-ext for FA or GA to be functors.  It is just typically
+/// true that FA and GA will also be functors.
+/// </remarks>
 /// <typeparam name="F">From functor</typeparam>
 /// <typeparam name="G">To functor</typeparam>
 public interface Natural<out F, in G>
