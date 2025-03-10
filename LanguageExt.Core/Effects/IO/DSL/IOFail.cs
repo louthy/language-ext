13149b9097d083362a4918832b1ba49991160616
Paul Louth <p.louth@gmail.com>
2025-03-10 20:35:30 +0000

    Extend IOFail with additional effect-handling methods
    Added overrides for various folding, retrying, and environmental functions, ensuring consistent failure propagation across all operations. These changes enhance the flexibility and completeness of the IOFail implementation.

--- LanguageExt.Core/Effects/IO/DSL/IOFail.cs
+++ LanguageExt.Core/Effects/IO/DSL/IOFail.cs
@@ -6,14 +6,127 @@ namespace LanguageExt.DSL;
 
 record IOFail<A>(Error Value) : InvokeSync<A>
 {
-    public override IO<B> Map<B>(Func<A, B> f) =>
+    public override A Invoke(EnvIO envIO) => 
+        Value.Throw<A>();
+    
+    public override IO<B> Map<B>(Func<A, B> f) => 
         new IOFail<B>(Value);
 
     public override IO<B> Bind<B>(Func<A, K<IO, B>> f) => 
         new IOFail<B>(Value);
+    
+    public override IO<S> Fold<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder) =>
+        new IOFail<S>(Value);
 
-    public override A Invoke(EnvIO envIO) => 
-        Value.Throw<A>();
+    public override IO<S> Fold<S>(
+        S initialState,
+        Func<S, A, S> folder) =>
+        new IOFail<S>(Value);
+
+    public override IO<S> FoldWhile<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder,
+        Func<S, bool> stateIs) =>
+        new IOFail<S>(Value);
+
+    public override IO<S> FoldWhile<S>(
+        S initialState,
+        Func<S, A, S> folder,
+        Func<S, bool> stateIs) =>
+        new IOFail<S>(Value);
+    
+    public override IO<S> FoldWhile<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder,
+        Func<A, bool> valueIs) =>
+        new IOFail<S>(Value);
+
+    public override IO<S> FoldWhile<S>(
+        S initialState,
+        Func<S, A, S> folder,
+        Func<A, bool> valueIs) =>
+        new IOFail<S>(Value);
+    
+    public override IO<S> FoldWhile<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder,
+        Func<(S State, A Value), bool> predicate) =>
+        new IOFail<S>(Value);
+
+    public override IO<S> FoldWhile<S>(
+        S initialState,
+        Func<S, A, S> folder,
+        Func<(S State, A Value), bool> predicate) =>
+        new IOFail<S>(Value);
+
+    public override IO<S> FoldUntil<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder,
+        Func<S, bool> stateIs) =>
+        new IOFail<S>(Value);
+    
+    public override IO<S> FoldUntil<S>(
+        S initialState,
+        Func<S, A, S> folder,
+        Func<S, bool> stateIs) =>
+        new IOFail<S>(Value);
+    
+    public override IO<S> FoldUntil<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder,
+        Func<A, bool> valueIs) =>
+        new IOFail<S>(Value);
+    
+    public override IO<S> FoldUntil<S>(
+        S initialState,
+        Func<S, A, S> folder,
+        Func<A, bool> valueIs) =>
+        new IOFail<S>(Value);
+    
+    public override IO<S> FoldUntil<S>(
+        S initialState,
+        Func<S, A, S> folder,
+        Func<(S State, A Value), bool> predicate) =>
+        new IOFail<S>(Value);
+
+    public override IO<S> FoldUntil<S>(
+        Schedule schedule,
+        S initialState,
+        Func<S, A, S> folder,
+        Func<(S State, A Value), bool> predicate) =>
+        new IOFail<S>(Value);
+    
+    public override IO<A> Post() =>
+        this;
+
+    protected override IO<A> WithEnv(Func<EnvIO, EnvIO> f) => 
+        this;
+
+    protected override IO<A> WithEnvFail(Func<EnvIO, EnvIO> f) => 
+        this;
+    
+    public override IO<ForkIO<A>> Fork(Option<TimeSpan> timeout = default) =>
+        new IOFail<ForkIO<A>>(Value);
+
+    public override IO<A> RepeatUntil(Schedule schedule, Func<A, bool> predicate) => 
+        this;
+
+    public override IO<A> RetryUntil(Schedule schedule, Func<Error, bool> predicate) => 
+        this;
+
+    public override IO<A> Finally<X>(K<IO, X> @finally) => 
+        this;
+
+    public override IO<A> Catch(Func<Error, bool> Predicate, Func<Error, K<IO, A>> Fail) =>
+        this;
     
     public override string ToString() => 
         $"fail({Value})";
