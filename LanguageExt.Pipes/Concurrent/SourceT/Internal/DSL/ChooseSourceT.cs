e4b5e235badd5cb7cf09398bf67248d047978515
Paul Louth <p.louth@gmail.com>
2025-04-16 13:09:59 +0100

    Refining the merging

--- LanguageExt.Pipes/Concurrent/SourceT/Internal/DSL/ChooseSourceT.cs
+++ LanguageExt.Pipes/Concurrent/SourceT/Internal/DSL/ChooseSourceT.cs
@@ -20,16 +20,13 @@ static class ChooseSourceT
     public static SourceTIterator<M, A> makeIterator<M, A>(Seq<SourceTIterator<M, A>> sources)
         where M : MonadIO<M>, Alternative<M>
     {
-        var envIO = EnvIO.New();
-        
         // Create a channel for the merged streams
-        var countdown = Signal.countdown<M>(sources.Count); 
-        var channel   = Channel.CreateUnbounded<K<M, A>>();
-        var writer    = channel.Writer;
+        var channel = Channel.CreateUnbounded<K<M, A>>();
+        var writer  = channel.Writer;
 
         // For each stream, reduce it (which writes to the merged stream), fork it, and return
         // This gives us K<M, A> that we can't run directly, so we must bind it...
-        var forks = from signal in countdown
+        var forks = from signal in Signal.countdown<M>(sources.Count)
                     let trigger = trigger<M, A>(signal, channel)
                     from result in sources.Map(s => s.ReduceM(unit, (_, ma) => writeAsync<M, K<M, A>>(writer, ma))
                                                      .Bind(_ => trigger)
@@ -43,7 +40,7 @@ static class ChooseSourceT
         var single = new LiftSourceT<M, Unit>(forks);
 
         // Then create a reader iterator that will yield the merged values 
-        var reader = new MergedReaderSourceTIterator<M, A>(channel.Reader, envIO);
+        var reader = new ReaderSourceTIterator<M, A>(channel.Reader);
 
         // Then bind them together so the forks launch, then the reader reads
         return new BindSourceTIterator<M, Unit, A>(single.GetIterator(), _ => reader);
