024e6909f4cfd5d1ed224bd5982fde2a9550da7a
Paul Louth <p.louth@gmail.com>
2025-05-05 11:53:15 +0100

    Minor

--- LanguageExt.Core/Monads/Alternative Monads/OptionT/OptionT.Monad.cs
+++ LanguageExt.Core/Monads/Alternative Monads/OptionT/OptionT.Monad.cs
@@ -23,7 +23,7 @@ public partial class OptionT<M> :
     static K<OptionT<M>, A> Applicative<OptionT<M>>.Pure<A>(A value) => 
         OptionT<M, A>.Some(value);
 
-    static K<OptionT<M>, B> Applicative<OptionT<M>>.Apply<A, B>(K<OptionT<M>, Func<A, B>> mf, K<OptionT<M>, A> ma) => 
+    static K<OptionT<M>, B> Applicative<OptionT<M>>.Apply<A, B>(K<OptionT<M>, Func<A, B>> mf, K<OptionT<M>, A> ma) =>
         mf.As().Bind(x => ma.As().Map(x));
 
     static K<OptionT<M>, B> Applicative<OptionT<M>>.Action<A, B>(K<OptionT<M>, A> ma, K<OptionT<M>, B> mb) =>
--- Samples/CardGame/Game.Monad/Game.Monad.cs
+++ Samples/CardGame/Game.Monad/Game.Monad.cs
@@ -4,7 +4,7 @@ namespace CardGame;
 
 public partial class Game :
     Deriving.Monad<Game, StateT<GameState, OptionT<IO>>>,
-    Deriving.SemigroupK<Game, StateT<GameState, OptionT<IO>>>,
+    Deriving.Choice<Game, StateT<GameState, OptionT<IO>>>,
     Deriving.Stateful<Game, StateT<GameState, OptionT<IO>>, GameState>
 {
     public static K<StateT<GameState, OptionT<IO>>, A> Transform<A>(K<Game, A> fa) =>
