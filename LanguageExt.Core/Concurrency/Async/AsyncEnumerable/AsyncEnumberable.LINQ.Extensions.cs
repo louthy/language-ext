7bd3d89e5cdf19ec8c270a044dba2d411f60cb83
Paul Louth <p.louth@gmail.com>
2025-11-13 13:29:52 +0000

    dotnet 10 version bump

--- LanguageExt.Core/Concurrency/Async/AsyncEnumerable/AsyncEnumberable.LINQ.Extensions.cs
+++ LanguageExt.Core/Concurrency/Async/AsyncEnumerable/AsyncEnumberable.LINQ.Extensions.cs
@@ -7,76 +7,67 @@ namespace LanguageExt.Async.Linq;
 
 public static class AsyncEnumerableExtensions
 {
-    public static async IAsyncEnumerable<B> Select<A, B>(this IAsyncEnumerable<A> ma, Func<A, B> f)
+    extension<A>(IAsyncEnumerable<A> ma)
     {
-        await foreach (var a in ma)
+        public async IAsyncEnumerable<B> Select<B>(Func<A, ValueTask<B>> f)
         {
-            yield return f(a);
-        }
-    }
-    
-    public static async IAsyncEnumerable<B> Select<A, B>(this IAsyncEnumerable<A> ma, Func<A, ValueTask<B>> f)
-    {
-        await foreach (var a in ma)
-        {
-            yield return await f(a);
-        }
-    }
-    
-    public static async IAsyncEnumerable<B> SelectMany<A, B>(this IAsyncEnumerable<A> ma, Func<A, IAsyncEnumerable<B>> f)
-    {
-        await foreach (var a in ma)
-        {
-            await foreach (var b in f(a))
+            await foreach (var a in ma)
             {
-                yield return b;
+                yield return await f(a);
             }
         }
-    }
-    
-    public static async IAsyncEnumerable<C> SelectMany<A, B, C>(
-        this IAsyncEnumerable<A> ma, 
-        Func<A, IAsyncEnumerable<B>> bind,
-        Func<A, B, C> project)
-    {
-        await foreach (var a in ma)
+
+        public async IAsyncEnumerable<B> SelectMany<B>(Func<A, IAsyncEnumerable<B>> f)
         {
-            await foreach (var b in bind(a))
+            await foreach (var a in ma)
             {
-                yield return project(a, b);
+                await foreach (var b in f(a))
+                {
+                    yield return b;
+                }
             }
         }
-    }
-        
-    public static async IAsyncEnumerable<A> Where<A>(
-        this IAsyncEnumerable<A> ma, 
-        Func<A, bool> f)
-    {
-        await foreach (var a in ma)
+
+        public async IAsyncEnumerable<C> SelectMany<B, C>(Func<A, IAsyncEnumerable<B>> bind,
+                                                          Func<A, B, C> project)
         {
-            if(f(a)) yield return a;
+            await foreach (var a in ma)
+            {
+                await foreach (var b in bind(a))
+                {
+                    yield return project(a, b);
+                }
+            }
         }
-    }
-        
-    public static async IAsyncEnumerable<A> Skip<A>(this IAsyncEnumerable<A> ma, int amount)
-    {
-        await foreach (var a in ma)
+
+        public async IAsyncEnumerable<A> Where(Func<A, bool> f)
         {
-            if (amount == 0)
-                yield return a;
-            else
-                amount--;
+            await foreach (var a in ma)
+            {
+                if(f(a)) yield return a;
+            }
         }
-    }
-        
-    public static async IAsyncEnumerable<A> Take<A>(this IAsyncEnumerable<A> ma, int amount)
-    {
-        await foreach (var a in ma)
+
+        public async IAsyncEnumerable<A> Skip(int amount)
         {
-            if (amount > 0) 
-                yield return a;
-            else
-                amount--;
+            await foreach (var a in ma)
+            {
+                if (amount == 0)
+                    yield return a;
+                else
+                    amount--;
+            }
+        }
+
+        public async IAsyncEnumerable<A> Take(int amount)
+        {
+            await foreach (var a in ma)
+            {
+                if (amount > 0) 
+                    yield return a;
+                else
+                    amount--;
+            }
         }
     }
 }
