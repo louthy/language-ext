dea58e1cc276d492f64d52f8dc668acf2fec6569
Paul Louth <p.louth@gmail.com>
2026-01-02 22:05:53 +0000

    Inlining attribute

--- LanguageExt.Core/Immutable Collections/Seq/Trait/Seq.FoldState.cs
+++ LanguageExt.Core/Immutable Collections/Seq/Trait/Seq.FoldState.cs
@@ -17,6 +17,7 @@ public partial class Seq
         int Index;
         readonly IEnumerator? Enum; 
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public FoldState(ref object span, int length, int index, IEnumerator? @enum)
         {
             Span = ref span;
@@ -25,15 +26,19 @@ public partial class Seq
             Enum = @enum;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void FromSpan<A>(ref FoldState state, ReadOnlySpan<A> span) =>
             state = new FoldState(ref Unsafe.As<A, object>(ref MemoryMarshal.GetReference(span)), span.Length, -1, null);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void FromSpanBack<A>(ref FoldState state, ReadOnlySpan<A> span) =>
             state = new FoldState(ref Unsafe.As<A, object>(ref MemoryMarshal.GetReference(span)), span.Length, span.Length, null);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void FromEnumerator(ref FoldState state, IEnumerator @enum) =>
             state = new FoldState(ref Unsafe.NullRef<object>(), 0, -1, @enum);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool MoveNext<A>(ref FoldState state, out A value)
         {
             if (state.Enum is null)
@@ -69,6 +74,7 @@ public partial class Seq
             }
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool MovePrev<A>(ref FoldState state, out A value)
         {
             ref var ix = ref state.Index;
