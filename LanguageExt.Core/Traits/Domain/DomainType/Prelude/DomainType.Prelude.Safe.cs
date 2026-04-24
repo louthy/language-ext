81b012d4dd3871f6e3433cd83db60a3e5bfb1aed
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-04-24 18:47:42 -0400

    Feature: Add safe/unsafe domain type constructors to Prelude
    Added NewM<T> and New<T> overloads to Prelude for multiple primitive and struct types, enabling safe (Fin<T>) and unsafe (T) construction of domain types via T.From and T.FromUnsafe. Applied generic constraints to ensure correct DomainType implementation.

---
+++ LanguageExt.Core/Traits/Domain/DomainType/Prelude/DomainType.Prelude.Safe.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using LanguageExt;
+using LanguageExt.Traits.Domain;
+
+namespace LanguageExt;
+
+public static partial class Prelude
+{
+    public static Fin<T> NewM<T>(bool repr)
+        where T : DomainType<T, bool> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(char repr)
+        where T : DomainType<T, char> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(string repr)
+        where T : DomainType<T, string> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(byte repr)
+        where T : DomainType<T, byte> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(short repr)
+        where T : DomainType<T, short> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(ushort repr)
+        where T : DomainType<T, ushort> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(int repr)
+        where T : DomainType<T, int> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(uint repr)
+        where T : DomainType<T, uint> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(long repr)
+        where T : DomainType<T, long> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(ulong repr)
+        where T : DomainType<T, ulong> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(float repr)
+        where T : DomainType<T, float> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(double repr)
+        where T : DomainType<T, double> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(decimal repr)
+        where T : DomainType<T, decimal> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(DateOnly repr)
+        where T : DomainType<T, DateOnly> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(TimeOnly repr)
+        where T : DomainType<T, TimeOnly> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(DateTime repr)
+        where T : DomainType<T, DateTime> =>
+        T.From(repr);
+
+    public static Fin<T> NewM<T>(DateTimeOffset repr)
+        where T : DomainType<T, DateTimeOffset> =>
+        T.From(repr);
+}
---
+++ LanguageExt.Core/Traits/Domain/DomainType/Prelude/DomainType.Prelude.Unsafe.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using LanguageExt.Traits.Domain;
+
+namespace LanguageExt;
+
+public static partial class Prelude
+{
+    public static T New<T>(bool repr)
+        where T : DomainType<T, bool> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(char repr)
+        where T : DomainType<T, char> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(string repr)
+        where T : DomainType<T, string> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(byte repr)
+        where T : DomainType<T, byte> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(short repr)
+        where T : DomainType<T, short> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(ushort repr)
+        where T : DomainType<T, ushort> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(int repr)
+        where T : DomainType<T, int> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(uint repr)
+        where T : DomainType<T, uint> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(long repr)
+        where T : DomainType<T, long> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(ulong repr)
+        where T : DomainType<T, ulong> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(float repr)
+        where T : DomainType<T, float> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(double repr)
+        where T : DomainType<T, double> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(decimal repr)
+        where T : DomainType<T, decimal> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(DateOnly repr)
+        where T : DomainType<T, DateOnly> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(TimeOnly repr)
+        where T : DomainType<T, TimeOnly> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(DateTime repr)
+        where T : DomainType<T, DateTime> =>
+        T.FromUnsafe(repr);
+
+    public static T New<T>(DateTimeOffset repr)
+        where T : DomainType<T, DateTimeOffset> =>
+        T.FromUnsafe(repr);
+
+}
