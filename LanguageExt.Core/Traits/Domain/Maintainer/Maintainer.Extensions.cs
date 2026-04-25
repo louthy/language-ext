54a6ed33b24eca8df5f5547d9f9cf84a3b4d5bb7
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-04-24 20:21:13 -0400

    Feature: Add Maintainer trait, extensions, and tests
    Introduced the Maintainer trait with generic interfaces for domain types and representations. Added extension methods and Prelude helpers for querying Maintainer instances. Implemented MichelinStart as a Maintainer example and added comprehensive tests to verify functionality.

---
+++ LanguageExt.Core/Traits/Domain/Maintainer/Maintainer.Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using LanguageExt.Traits.Domain;
+
+namespace LanguageExt;
+
+public static class MaintainerExtensions
+{
+    extension<A>(A)
+        where A : Maintainer<A>
+    {
+        public static Option<A> FindM(Func<A, bool> fa) =>
+            Prelude.findM(fa);
+
+        public static A Find(Func<A, bool> fa) =>
+            Prelude.find(fa);
+    }
+
+    extension<A>(A a)
+        where A : Maintainer<A>
+    {
+        public bool Is(A b) => a.Equals(b);
+        public bool IsNot(A b) => !a.Equals(b);
+    }
+
+}
+
