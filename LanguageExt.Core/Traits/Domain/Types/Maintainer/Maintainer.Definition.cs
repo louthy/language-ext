cca8746c8e027df82d717d62a6c5c80073f1ecac
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-05-04 18:50:52 -0400

    feat: moved Maintainer to Domain/Types

--- LanguageExt.Core/Traits/Domain/Types/Maintainer/Maintainer.Definition.cs
+++ LanguageExt.Core/Traits/Domain/Types/Maintainer/Maintainer.Definition.cs
@@ -1,12 +1,14 @@
 ﻿namespace LanguageExt.Traits.Domain;
 
-public interface Maintainer<SELF> : DomainType<SELF>
+public interface Maintainer<SELF> : 
+    DomainSet<SELF>
     where SELF : Maintainer<SELF>
 {
     static abstract Seq<SELF> All { get; }
 }
 
-public interface Maintainer<SELF, REPR> : Maintainer<SELF>
+public interface Maintainer<SELF, REPR> : 
+    Maintainer<SELF>
     where SELF : Maintainer<SELF, REPR>
 {
     REPR To();
